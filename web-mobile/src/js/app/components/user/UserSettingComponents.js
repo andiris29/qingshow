30bf5f76e69675eef2beb92c3055397ee770bd8f
HashMap <sebastianqu@gmail.com>
2014-10-10 01:37:14 +0800

    User Settings implement

--- web-mobile/src/js/app/components/user/UserSettingComponents.js
+++ web-mobile/src/js/app/components/user/UserSettingComponents.js
@@ -5,7 +5,23 @@ define([
 ], function(UIComponent, TemplateManager, DataService) {
 // @formatter:on
   
+    /**
+     * User Settings's UI Componets
+     */
     var UserSettingComponents = function(dom) {
+        UserSettingComponents.superclass.constructor.apply(this, arguments);
+
+        async.parallel([ function(callback) {
+            // load template
+            TemplateManager.load('user/user-Settings', true, function(err, content$) {
+                this._dom$.append(content);
+                callback(null);
+            }.bind(this));
+        }.bind(this)], function(err, results) {
+            var main$ = results[1];
+            main$.appendTo($('.qsTbltUserSettingMain', this._dom$));
+            this._render(results[2]);
+        }.bind(this));
       
     };
     andrea.oo.extend(UserSettingComponents, UIComponent);
