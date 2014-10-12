e79688367e2b6ed853beee17cdd999f4e7d19052
HashMap <sebastianqu@gmail.com>
2014-10-13 01:51:31 +0800

    user saveable header

--- web-mobile/src/js/app/views/user/U02UserSetting.js
+++ web-mobile/src/js/app/views/user/U02UserSetting.js
@@ -2,9 +2,9 @@
 define([
     'ui/scroll/IScrollContainer',
     'app/views/ViewBase',
-    'app/components/header/BackableHeader',
+    'app/components/header/SaveableHeader',
     'app/components/user/UserSettingComponents'
-], function(IScrollContainer, ViewBase, BackableHeader, UserSettingComponents) {
+], function(IScrollContainer, ViewBase, SaveableHeader, UserSettingComponents) {
 // @formatter:on
     /**
      * The top level dom element, which will fit to screen
@@ -12,7 +12,7 @@ define([
     var U02UserSetting = function(dom) {
         U02UserSetting.superclass.constructor.apply(this, arguments);
 
-        var header = new BackableHeader($('<div/>').appendTo(this._dom$), '设置');
+        var header = new SaveableHeader($('<div/>').appendTo(this._dom$), '设置');
 
         var body = new IScrollContainer($('<div/>').css({
             'width' : '100%',
