2fe2431f2674a60bc7e78ba827b0e3dc131b4778
wxy325 <wxy325@qq.com>
2014-10-12 12:16:07 +0800

    node-server:implement feeding/byRecommendation

--- server-node/lib/httpserver/services/feeding.js
+++ server-node/lib/httpserver/services/feeding.js
@@ -1,34 +1,52 @@
 var Show = require('../../model/shows');
 
+
 var _byRecommendation;
 _byRecommendation = function (req, res) {
-    var param, tags, pageNo, pageSize;
+    var param, tags, pageNo, pageSize, query;
     param = req.body;
     tags = param.tags || [];
     pageNo = param.pageNo || 1;
     pageSize = param.pageSize || 10;
 
+    function buildQuery(){
+        query = Show.find();
+        if (tags.length){
+            query.where({tags: {$in: tags}});
+        }
+        return query;
+    }
 
-    Show.find({tags: {$in: tags}})
-        .skip((pageNo - 1) * pageSize)
-        .limit(pageSize)
-        .sort({numLike: 1})
-        .exec(function (err, shows) {
-            if (err) {
-                res.send('error');
-            } else {
-                var retData = {
-                    metadata: {
-                        //TODO change invilidateTime
-                        invalidateTime: 30
-                    },
-                    data: {
-                        shows: shows
-                    }
-                };
-                res.json(retData);
-            }
-        });
+    query = buildQuery();
+    query.count(function(err, count){
+        var numPages = parseInt((count + pageSize - 1) / pageSize);
+        query = buildQuery();
+
+        query.skip((pageNo - 1) * pageSize)
+            .limit(pageSize)
+            .sort({numLike: 1})
+            .populate("producerRef")
+            .populate("itemRefs")
+            .exec(function (err, shows) {
+                if (err) {
+                    res.send('error');
+                } else {
+                    var retData = {
+                        metadata: {
+                            //TODO change invilidateTime
+                            "numPages": numPages,
+                            "invalidateTime": 3600000
+                        },
+                        data: {
+                            shows: shows
+                        }
+                    };
+                    res.json(retData);
+                }
+            });
+    });
+
+
 };
 
 module.exports = {
