4a325d725da0bd3304630e8a9ca225997350a3ba
HashMap <sebastianqu@gmail.com>
2014-10-15 02:44:52 +0800

    hair type implement

--- web-mobile/src/js/app/components/user/HairTypeComponent.js
+++ web-mobile/src/js/app/components/user/HairTypeComponent.js
@@ -3,32 +3,51 @@ define([
     'ui/UIComponent',
     'app/managers/TemplateManager',
     'app/services/DataService',
+    'app/utils/CodeUtils',
     'app/utils/RenderUtils'
-], function(UIComponent, TemplateManager, DataService, RenderUtils) {
+], function(UIComponent, TemplateManager, DataService, CodeUtils, RenderUtils) {
 // @formatter:on
 
-    var HairTypeComponent = function(dom, people) {
+    var HairTypeComponent = function(dom) {
         HairTypeComponent.superclass.constructor.apply(this, arguments);
 
+        console.log(this._data);
         async.parallel([function(callback) {
             // load template
             TemplateManager.load('user/hair-type.html', true, function(err, content$) {
                 this._dom$.append(content$);
                 callback(null);
             }.bind(this));
-        }.bind(this), function(callback) {
-            // load data
         }.bind(this)], function(callback) {
-            this._render(people);
+            this._render();
         }.bind(this));
 
     };
 
     andrea.oo.extend(HairTypeComponent, UIComponent);
 
-    HairTypeComponent.prototype._render = function(people) {
+    HairTypeComponent.prototype._render = function() {
+        var hairType = CodeUtils.getCodes('people.hairType');
+
+        var view$ = $('.qsTpltHairTypeMain', this._dom$);
+        var contain$ = $('.qsListInfoSection', view$);
+        var tplt$ = $('.qsHidden', view$);
+
+        hairType.forEach(function(hair, index) {
+            var li$ = $(".qsRow", tplt$).clone();
+            $('.qsTitle', li$).text(hair);
+            $('div:last', li$).toggleClass('hairType-'+index);
+            li$.appendTo(contain$);
+        });
+
+        this._data._user.hairTypes.forEach(function(element) {
+            $('.hairType-' + element, contain$).removeClass('fa-check-circle2');
+            $('.hairType-' + element, contain$).toggleClass('fa-check-circle-o2');
+        });
     };
 
     HairTypeComponent.prototype.save = function() {
     };
+
+    return HairTypeComponent;
 });
