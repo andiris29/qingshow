890a57a2eef2bfb1ec7be6323eaed7d7a7c24830
andiris29 <andiris29@gmail.com>
2014-10-22 01:13:10 +0800

    Add U01User

--- web-mobile/src/js/app/views/user/U01User.js
+++ web-mobile/src/js/app/views/user/U01User.js
@@ -1,11 +1,13 @@
 // @formatter:off
 define([
     'ui/containers/IScrollContainer',
+    'app/services/FeedingService',
     'app/views/ViewBase',
     'app/components/common/Header',
-    'app/components/show/Show',
+    'app/components/common/Navigator',
+    'app/components/user/User',
     'app/components/show/ShowGallery'
-], function(IScrollContainer, ViewBase, Header, ItemMain, ItemGallery) {
+], function(IScrollContainer, FeedingService, ViewBase, Header, Navigator, User, ShowGallery) {
 // @formatter:on
     /**
      * The top level dom element, which will fit to screen
@@ -13,7 +15,35 @@ define([
     var U01User = function(dom) {
         U01User.superclass.constructor.apply(this, arguments);
 
-        var header = new Header($('<div/>').appendTo(this._dom$), '用户 xxx');
+        var header = new Header($('<div/>').appendTo(this._dom$), {
+            'right' : '设置'
+        });
+        header.on('clickRight', function() {
+            appRuntime.view.to('app/views/user/U02UserSetting');
+        });
+        var body = new IScrollContainer($('<div/>').css({
+            'width' : '100%',
+            'height' : this._dom$.height() - header.getPreferredSize().height
+        }).appendTo(this._dom$));
+
+        var user = new User($('<div/>'), this._model);
+        body.append(user);
+
+        var navi = new Navigator($('<div/>'));
+        navi.append(new ShowGallery($('<div/>'), {
+            'feeding' : FeedingService.choosen
+        }));
+        navi.append(new ShowGallery($('<div/>'), {
+            'feeding' : FeedingService.choosen
+        }));
+        navi.append(new ShowGallery($('<div/>'), {
+            'feeding' : FeedingService.choosen
+        }));
+        body.append(navi);
+        //
+        user.on('selectTab', function(event, index) {
+            navi.index(index);
+        });
     };
     andrea.oo.extend(U01User, ViewBase);
 
