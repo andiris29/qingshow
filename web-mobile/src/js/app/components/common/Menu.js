dd0365fc5f4ee7d2143a0221b96b3c1f17b80ab7
andiris29 <andiris29@gmail.com>
2014-11-10 23:06:19 +0800

    Add comparison mockup

--- web-mobile/src/js/app/components/common/Menu.js
+++ web-mobile/src/js/app/components/common/Menu.js
@@ -17,31 +17,43 @@ define([
             $('li').on(appRuntime.events.click, function(event) {
                 var selected$ = $(event.currentTarget), feeding;
 
-                if ($('.qsHot', selected$).length) {
-                    if ($('.qsPromotion', selected$).length) {
-                        feeding = FeedingService.chosenByPromotion;
-                    } else {
-                        if (!$('.qsModels', selected$).length) {
-                            feeding = FeedingService.hot;
-                        }
+                for (var selector in _itemMetadata) {
+                    if ($(selector, selected$).length) {
+                        var metadata = _itemMetadata[selector];
+                        appRuntime.view.to(metadata.clazz, $.extend({
+                            'title' : $('.qsCategoryText', selected$).text()
+                        }, metadata.data));
+                        break;
                     }
-                } else if ($('.qsComparison', selected$).length) {
-                    // TODO
-                } else if ($('.qsBag', selected$).length) {
-                    feeding = FeedingService.tagBag;
-                } else if ($('.qsAccessories', selected$).length) {
-                    feeding = FeedingService.tagAccesories;
-                } else if ($('.qsShoe', selected$).length) {
-                    feeding = FeedingService.tagShoe;
-                } else if ($('.qsCosmetic', selected$).length) {
-                    feeding = FeedingService.studio;
                 }
-                if (feeding) {
-                    appRuntime.view.to('app/views/show/S02Feeding', {
-                        'feeding' : feeding,
-                        'title' : $('.qsCategoryText', selected$).text()
-                    });
-                }
+                // if ($('.qsHot', selected$).length) {
+                // if ($('.qsPromotion', selected$).length) {
+                // feeding = FeedingService.chosenByPromotion;
+                // } else {
+                // if (!$('.qsModels', selected$).length) {
+                // feeding = FeedingService.hot;
+                // }
+                // }
+                // } else if ($('.qsComparison', selected$).length) {
+                // appRuntime.view.to('app/views/show/S03Comparison', {
+                // 'feeding' : feeding,
+                // 'title' : $('.qsCategoryText', selected$).text()
+                // });
+                // } else if ($('.qsBag', selected$).length) {
+                // feeding = FeedingService.tagBag;
+                // } else if ($('.qsAccessories', selected$).length) {
+                // feeding = FeedingService.tagAccesories;
+                // } else if ($('.qsShoe', selected$).length) {
+                // feeding = FeedingService.tagShoe;
+                // } else if ($('.qsCosmetic', selected$).length) {
+                // feeding = FeedingService.studio;
+                // }
+                // if (feeding) {
+                // appRuntime.view.to('app/views/show/S02Feeding', {
+                // 'feeding' : feeding,
+                // 'title' : $('.qsCategoryText', selected$).text()
+                // });
+                // }
                 appRuntime.popup.remove(this);
             }.bind(this));
         }.bind(this));
@@ -49,6 +61,51 @@ define([
 
     andrea.oo.extend(Menu, UIComponent);
 
+    var _itemMetadata = {
+        '.qsHot.qsPromotion' : {
+            'clazz' : 'app/views/show/S02Feeding',
+            'data' : {
+                'feeding' : FeedingService.chosenByPromotion
+            }
+        },
+        '.qsHot:not(.qsModels)' : {
+            'clazz' : 'app/views/show/S02Feeding',
+            'data' : {
+                'feeding' : FeedingService.hot
+            }
+        },
+        '.qsComparison' : {
+            'clazz' : 'app/views/show/S03Comparison',
+            'data' : {
+                // TODO
+                'feeding' : FeedingService.chosenByEditor
+            }
+        },
+        '.qsBag' : {
+            'clazz' : 'app/views/show/S02Feeding',
+            'data' : {
+                'feeding' : FeedingService.tagBag
+            }
+        },
+        '.qsAccessories' : {
+            'clazz' : 'app/views/show/S02Feeding',
+            'data' : {
+                'feeding' : FeedingService.tagAccesories
+            }
+        },
+        '.qsShoe' : {
+            'clazz' : 'app/views/show/S02Feeding',
+            'data' : {
+                'feeding' : FeedingService.tagShoe
+            }
+        },
+        '.qsCosmetic' : {
+            'clazz' : 'app/views/show/S02Feeding',
+            'data' : {
+                'feeding' : FeedingService.studio
+            }
+        }
+    };
     Menu.prototype.getPreferredSize = function() {
         return {
             'width' : 640,
