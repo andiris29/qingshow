af02961573cb053e6306b359f350760452824f80
HashMap <sebastianqu@gmail.com>
2014-11-12 02:15:56 +0800

    ios-native: do U06

--- app-ios-native/qingshow-ios-native/vc/user/login/QSU06LoginViewController.m
+++ app-ios-native/qingshow-ios-native/vc/user/login/QSU06LoginViewController.m
@@ -9,7 +9,9 @@
 #import "QSU06LoginViewController.h"
 
 @interface QSU06LoginViewController ()
-
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
+@property (weak, nonatomic) IBOutlet UITextField *userText;
+@property (weak, nonatomic) IBOutlet UITextField *passwordText;
 @end
 
 @implementation QSU06LoginViewController
@@ -18,8 +20,10 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     
+    // View全体
     self.view.backgroundColor=[UIColor colorWithRed:240.f/255.f green:240.f/255.f blue:240.f/255.f alpha:1.f];
 
+    // Navibar
     self.navigationItem.title = @"登陆";
     self.navigationItem.backBarButtonItem.title = @"";
     UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithTitle:@" " style:UIBarButtonItemStyleDone target:nil action:nil];
@@ -33,6 +37,8 @@
     [[self navigationItem] setRightBarButtonItem:btnSave];
 
 
+    // 登陆
+    self.loginButton.backgroundColor = [UIColor colorWithRed:251.f/255.f green:145.f/255.f blue:95.f/255.f alpha:1.f];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -42,6 +48,14 @@
 }
 
 #pragma mark - Action
+
+- (IBAction)login:(id)sender {
+    NSLog(@"login to qingshow");
+}
+
+
+#pragma mark - Callback
+
 - (void)gotoRegister {
     
 }
