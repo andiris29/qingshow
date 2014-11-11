3143841c02138dd65407dfe3682dab318ae942d7
wxy325 <wxy325@qq.com>
2014-11-11 22:42:23 +0800

    ios-native: s03成形

--- app-ios-native/qingshow-ios-native/vc/show/detail/QSS03ShowDetailViewController.h
+++ app-ios-native/qingshow-ios-native/vc/show/detail/QSS03ShowDetailViewController.h
@@ -13,6 +13,21 @@
 @property (weak, nonatomic) IBOutlet UIScrollView *containerScrollView;
 @property (strong, nonatomic) IBOutlet UIView *contentView;
 
+@property (weak, nonatomic) IBOutlet UIView *showContainer;
+- (IBAction)playBtnPressed:(id)sender;
+- (IBAction)commentBtnPressed:(id)sender;
+- (IBAction)shareBtnPressed:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIImageView *headIconImageView;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *detailLabel;
+@property (weak, nonatomic) IBOutlet UILabel *contentLabel;
+@property (weak, nonatomic) IBOutlet UIView *itemContainer;
+@property (weak, nonatomic) IBOutlet UIButton *commentBtn;
+@property (weak, nonatomic) IBOutlet UIButton *favorBtn;
+- (IBAction)favorBtnPressed:(id)sender;
+@property (weak, nonatomic) IBOutlet UILabel *favorNumberLabel;
+
 - (id)init;
 
 @end
