e42ca1af55883e23714ec1a39d1cd7e09de91bb3
HashMap <sebastianqu@gmail.com>
2014-11-15 16:34:44 +0800

    add Utility Class for Validation

---
+++ app-ios-native/qingshow-ios-native/utility/extension/UIViewController+Utility.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+Utility.h
+//  qingshow-ios-native
+//
+//  Created by 瞿盛 on 14/11/15.
+//  Copyright (c) 2014年 QS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (Utility)
+
+@end
--- app-ios-native/qingshow-ios-native/vc/user/login/QSU07RegisterViewController.m
+++ app-ios-native/qingshow-ios-native/vc/user/login/QSU07RegisterViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "QSU07RegisterViewController.h"
+#import "UIViewController+ShowHud.h"
 
 @interface QSU07RegisterViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *accountText;
+@property (weak, nonatomic) IBOutlet UITextField *passwdText;
+@property (weak, nonatomic) IBOutlet UITextField *passwdCfmText;
+@property (weak, nonatomic) IBOutlet UIButton *registerButton;
 
 @end
 
@@ -17,6 +22,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    // View全体
+    self.view.backgroundColor=[UIColor colorWithRed:240.f/255.f green:240.f/255.f blue:240.f/255.f alpha:1.f];
+    
+    // Navibar
+    self.navigationItem.title = @"注册";
+    self.navigationItem.backBarButtonItem.title = @"";
+    UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithTitle:@" " style:UIBarButtonItemStyleDone target:nil action:nil];
+    [[self navigationItem] setBackBarButtonItem:backButton];
+    
+//    UIBarButtonItem *btnSave = [[UIBarButtonItem alloc]initWithTitle:@"注册"
+//                                                               style:UIBarButtonItemStylePlain
+//                                                              target:self
+//                                                              action:@selector(gotoRegister)];
+//    
+//    [[self navigationItem] setRightBarButtonItem:btnSave];
+//    
+//    
+    // 登陆
+    self.registerButton.backgroundColor = [UIColor colorWithRed:251.f/255.f green:145.f/255.f blue:95.f/255.f alpha:1.f];
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +50,27 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
+# pragma mark - Action
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)register:(id)sender {
+
+    NSString *account = self.accountText.text;
+    NSString *passwd = self.passwdText.text;
+    NSString *passwdCfm = self.passwdCfmText.text;
+    
+    if (account.length == 0) {
+        [self showErrorHudWithText:@"请输入账号"];
+        return;
+    }
+    
+    if (passwd.length == 0) {
+        [self showErrorHudWithText:@"请输入账号"];
+        return;
+    }
 }
-*/
+
+# pragma mark - private
+
+
 
 @end
