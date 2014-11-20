475f218831642098b757bc954364f07cf2b0ba1e
wxy325 <wxy325@qq.com>
2014-11-21 00:51:12 +0800

    ios-native: adjust image size of collection view cell

--- app-ios-native/qingshow-ios-native/utility/type/ServerPath.h
+++ app-ios-native/qingshow-ios-native/utility/type/ServerPath.h
@@ -9,7 +9,8 @@
 #ifndef qingshow_ios_native_ServerPath_h
 #define qingshow_ios_native_ServerPath_h
 
-#define HOST_ADDRESS @"localhost:30001/"
+//#define HOST_ADDRESS @"localhost:30001/"
+#define HOST_ADDRESS @"121.41.162.102:30001/"
 #define HOST_NAME [NSString stringWithFormat:@"%@%@",HOST_ADDRESS, @"services"]
 #define kImageUrlBase [NSString stringWithFormat:@"http://%@%@",HOST_ADDRESS, @"images"]
 #define kVideoUrlBase [NSString stringWithFormat:@"http://%@%@",HOST_ADDRESS, @"videos"]
