74688fe55bb7f2fef84bba25aee0684b3db18d47
HashMap <sebastianqu@gmail.com>
2014-11-23 01:55:07 +0800

    ios-native: 修改U05的实现方式

---
+++ app-ios-native/qingshow-ios-native/vc/user/setting/QSU05HairGenderTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  QSU05HairGenderTableViewController.h
+//  qingshow-ios-native
+//
+//  Created by 瞿盛 on 14/11/23.
+//  Copyright (c) 2014年 QS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QSU05HairGenderTableViewController : UITableViewController
+
+@end
