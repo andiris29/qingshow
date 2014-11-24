7dbd5859e502b20be28fb5fd8e9ff389eb9c2b8a
Zenan <twolittlecats@163.com>
2014-11-24 22:52:05 +0800

    添加login register网络请求

--- app-android-native/app/src/main/java/com/focosee/qingshow/activity/U06LoginActivity.java
+++ app-android-native/app/src/main/java/com/focosee/qingshow/activity/U06LoginActivity.java
@@ -1,8 +1,10 @@
 package com.focosee.qingshow.activity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,7 +12,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.focosee.qingshow.R;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class U06LoginActivity extends Activity {
@@ -18,16 +30,22 @@ public class U06LoginActivity extends Activity {
     private Button submitButton;
     private EditText accountEditText;
     private EditText passwordEditText;
+    private Context context;
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        context = getApplicationContext();
+
         registerTextView = (TextView) findViewById(R.id.registerTextView);
         submitButton = (Button) findViewById(R.id.submitButton);
         accountEditText = (EditText) findViewById(R.id.accountEditText);
         passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+
+        requestQueue = Volley.newRequestQueue(context);
 
         registerTextView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,10 +54,36 @@ public class U06LoginActivity extends Activity {
                 startActivity(intent);
             }
         });
+
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                        "http://121.41.162.102:30001/services/user/login",
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                Log.d("TAG", response);
+                                Intent intent = new Intent(U06LoginActivity.this, U01PersonalActivity.class);
+                                startActivity(intent);
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("TAG", error.getMessage(), error);
+                    }
+                }) {
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> map = new HashMap<String, String>();
 
+                        map.put("mail", accountEditText.getText().toString());
+                        map.put("password", passwordEditText.getText().toString());
+                        return map;
+                    }
+                };
+
+                requestQueue.add(stringRequest);
             }
         });
     }
--- app-android-native/app/src/main/java/com/focosee/qingshow/activity/U07RegisterActivity.java
+++ app-android-native/app/src/main/java/com/focosee/qingshow/activity/U07RegisterActivity.java
@@ -1,19 +1,90 @@
 package com.focosee.qingshow.activity;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.focosee.qingshow.R;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class U07RegisterActivity extends Activity {
+    private RequestQueue requestQueue;
+    private Button submitButton;
+    private EditText accountEditText;
+    private EditText passwordEditText;
+    private EditText confirmEditText;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        context = getApplicationContext();
+
+        submitButton = (Button) findViewById(R.id.submitButton);
+        accountEditText = (EditText) findViewById(R.id.accountEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+        confirmEditText = (EditText) findViewById(R.id.confirmEditText);
+
+        requestQueue = Volley.newRequestQueue(context);
+
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (passwordEditText.getText().toString().equals(confirmEditText.getText().toString())) {
+                    Toast.makeText(context, "请确认两次密码", Toast.LENGTH_LONG).show();
+                }
+
+                StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                        "http://121.41.162.102:30001/services/user/register",
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                Log.d("TAG", response);
+                                Intent intent = new Intent(U07RegisterActivity.this, U01PersonalActivity.class);
+                                startActivity(intent);
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("TAG", error.getMessage(), error);
+                    }
+                }) {
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> map = new HashMap<String, String>();
+
+                        map.put("mail", accountEditText.getText().toString());
+                        map.put("password", passwordEditText.getText().toString());
+                        return map;
+                    }
+                };
+
+                requestQueue.add(stringRequest);
+            }
+        });
     }
 
     @Override
