f07d2f1e499f3db13ecceef62f683052524f820d
Zenan <twolittlecats@163.com>
2014-11-29 16:36:14 +0800

    判断登陆是否成功，service url改到config file

--- app-android-native/app/src/main/java/com/focosee/qingshow/config/QSAppWebAPI.java
+++ app-android-native/app/src/main/java/com/focosee/qingshow/config/QSAppWebAPI.java
@@ -4,6 +4,9 @@ package com.focosee.qingshow.config;
  * Created by jackyu on 11/22/14.
  */
 public class QSAppWebAPI {
+    public static final String LOGIN_SERVICE_URL = "http://121.41.162.102:30001/services/user/login";
+    public static final String REGISTER_SERVICE_URL = "http://121.41.162.102:30001/services/user/register";
+
     private static final String SITE_BASE = "http://chingshow.com:30001";
         private static final String SHOW_LIST_API = SITE_BASE + "/services/feeding/chosen?pageNo=";
 //    private static final String SHOW_LIST_API = SITE_BASE + "/services/feeding/hot?pageNo=";
