1c396c082d222ced436ba2a071ed167c56e24b88
Zenan <twolittlecats@163.com>
2014-11-30 13:53:14 +0800

    更改imageindicator sdk 20 to sdk 19

--- app-android-native/app/src/main/java/com/focosee/qingshow/config/QSAppWebAPI.java
+++ app-android-native/app/src/main/java/com/focosee/qingshow/config/QSAppWebAPI.java
@@ -6,10 +6,11 @@ package com.focosee.qingshow.config;
 public class QSAppWebAPI {
     public static final String LOGIN_SERVICE_URL = "http://121.41.162.102:30001/services/user/login";
     public static final String REGISTER_SERVICE_URL = "http://121.41.162.102:30001/services/user/register";
+    public static final String UPDATE_SERVICE_URL = "http://121.41.162.102:30001/services/user/update";
 
     private static final String SITE_BASE = "http://chingshow.com:30001";
-        private static final String SHOW_LIST_API = SITE_BASE + "/services/feeding/chosen?pageNo=";
-//    private static final String SHOW_LIST_API = SITE_BASE + "/services/feeding/hot?pageNo=";
+    private static final String SHOW_LIST_API = SITE_BASE + "/services/feeding/chosen?pageNo=";
+//  private static final String SHOW_LIST_API = SITE_BASE + "/services/feeding/hot?pageNo=";
 
 
     public static String getShowListApi(int pageIndex, int pageSize) {
