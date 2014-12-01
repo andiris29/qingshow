6c3ddb0140837600578930467c725519751389c3
Zenan <twolittlecats@163.com>
2014-12-01 17:15:26 +0800

    add pototype of settings

---
+++ app-android-native/app/src/main/java/com/focosee/qingshow/activity/U02TermsFragment.java
@@ -0,0 +1,31 @@
+package com.focosee.qingshow.activity;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.focosee.qingshow.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class U02TermsFragment extends Fragment {
+
+
+    public U02TermsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_u02_terms, container, false);
+    }
+
+
+}
