1b5fc4f1613e3a0f69fcb6bb2dfeea2ab30bd2e0
Zenan <twolittlecats@163.com>
2014-12-07 22:48:12 +0800

    完善settings

--- app-android-native/app/src/main/java/com/focosee/qingshow/activity/U02AboutVIPFragment.java
+++ app-android-native/app/src/main/java/com/focosee/qingshow/activity/U02AboutVIPFragment.java
@@ -1,24 +1,29 @@
 package com.focosee.qingshow.activity;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 import com.focosee.qingshow.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class U02AboutVIPFragment extends Fragment {
+import org.w3c.dom.Text;
 
+public class U02AboutVIPFragment extends Fragment {
+    private Context context;
+    private RequestQueue requestQueue;
+
+    private TextView backTextView;
 
     public U02AboutVIPFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,5 +32,19 @@ public class U02AboutVIPFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_u02_about_vi, container, false);
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        context = (Context) getActivity().getApplicationContext();
+        requestQueue = Volley.newRequestQueue(context);
 
+        backTextView = (TextView) getActivity().findViewById(R.id.backTextView);
+        backTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                U02SettingsFragment settingsFragment = new U02SettingsFragment();
+                getFragmentManager().beginTransaction().replace(R.id.settingsScrollView, settingsFragment).commit();
+            }
+        });
+    }
 }
