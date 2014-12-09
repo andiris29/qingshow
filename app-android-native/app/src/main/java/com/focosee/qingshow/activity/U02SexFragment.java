d53a86aa3c51476e3cbbc04ad91dbd4274722e19
Zenan <2smallcats@gmail.com>
2014-12-09 21:53:24 +0800

    修改sex pages

--- app-android-native/app/src/main/java/com/focosee/qingshow/activity/U02SexFragment.java
+++ app-android-native/app/src/main/java/com/focosee/qingshow/activity/U02SexFragment.java
@@ -3,27 +3,14 @@ package com.focosee.qingshow.activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.focosee.qingshow.R;
-import com.focosee.qingshow.config.QSAppWebAPI;
-
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class U02SexFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
@@ -63,7 +50,7 @@ public class U02SexFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_sex, container, false);
+        return inflater.inflate(R.layout.fragment_u02_sex, container, false);
     }
 
     @Override
