a8ae044b97ff8dc0cb509cbb93dd9588b03ccfd1
andiris29 <andiris29@gmail.com>
2015-01-30 17:00:41 +0800

    Update TODO description

--- goblin-tbshop/contentScripts/taobao.com/$shop/shop/view_shop.js
+++ goblin-tbshop/contentScripts/taobao.com/$shop/shop/view_shop.js
@@ -1,6 +1,9 @@
 /**
- * url: http://aoyp.taobao.com/shop/view_shop.htm?spm=a217f.1226604.1998111760.148.zFYPZO&user_number_id=94781032&ssid=r11
+ * url:
+ * http://aoyp.taobao.com/shop/view_shop.htm?spm=a217f.1226604.1998111760.148.zFYPZO&user_number_id=94781032&ssid=r11
  *
- * Redirect to http://$shop.taobao.com/search.htm?search=y&orderType=hotsell_desc
+ * Redirect to http://shop$shopId.taobao.com/search.htm?search=y&orderType=hotsell_desc
  */
 // TODO
+// Parst shopId in dom, eg: "shopId":"35593924"
+// eg: http://shop35593924.taobao.com/search.htm?search=y&orderType=hotsell_desc
