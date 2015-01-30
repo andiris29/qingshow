016775dee763a1c2bfc671498ad7c2e4152737a9
andiris29 <andiris29@gmail.com>
2015-01-30 10:33:15 +0800

    Recover nvzhuang spider

--- goblin-tbshop/contentScripts/taobao.com/www/market/nvzhuang.js
+++ goblin-tbshop/contentScripts/taobao.com/www/market/nvzhuang.js
@@ -8,17 +8,36 @@ var api = new RuntimeAPI({
         var body$ = $(document.body);
         body$.scrollTop(body$.height());
 
-        // TODO
         new PageManager();
     }
 });
 
 var PageManager = function() {
-    this._currentPage = null;
+    this._currentPage = new Page();
 };
 
-var Page = function() {
-    // $('.item.next')
-    // '.next-disabled'
-    // $('.m-itemList .items li')
-};
+var Page = function(successCallback, errorCallback) {
+    async.waterfall([
+    function(callback) {
+        var retry = 0;
+        var load = function() {
+            lis$ = $('.m-itemList .items li');
+            if (lis$.length === 0) {
+                if (retry < 10) {
+                    retry++;
+                    _.delay(load, 1000);
+                } else {
+                    callback('load fail.');
+                }
+            } else {
+                callback(null, lis$);
+            }
+        };
+    },
+    function(lis$) {
+        // $('.item.next')
+        // '.next-disabled'
+    }], function(err) {
+
+    });
+}; 
