aeb81b1a875182185ddd89cb81dd8ab493b97466
wxy325 <wxy325@qq.com>
2015-02-09 10:26:19 +0800

    Add image resizer

---
+++ imgResize/src/com/focosee/qingshow/FileWalker.java
@@ -0,0 +1,25 @@
+package com.focosee.qingshow;
+
+import java.io.File;
+
+/**
+ * Created by wxy325 on 2/8/15.
+ */
+public class FileWalker {
+    public static void walkThroughDirectory(String directoryPath, IFileHandler handler) {
+        File f = new File(directoryPath);
+
+        if (!f.isDirectory()) {
+            return;
+        }
+
+        File[] fList = f.listFiles();
+        for (File file : fList) {
+            if (file.isDirectory()) {
+                FileWalker.walkThroughDirectory(file.getAbsolutePath(), handler);
+            } else {
+                handler.handleFile(file);
+            }
+        }
+    }
+}
---
+++ imgResize/src/com/focosee/qingshow/ImageResizer.java
@@ -0,0 +1,30 @@
+package com.focosee.qingshow;
+import org.imgscalr.*;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+/**
+ * Created by wxy325 on 2/8/15.
+ */
+public class ImageResizer {
+    public static void resizeImage(String imageName, String destName) {
+        try {
+            File file = new File(imageName);
+            BufferedImage img = ImageIO.read(file);
+            int width = img.getWidth(null);
+            int height = img.getHeight(null);
+
+            BufferedImage resizedImage = Scalr.resize(img, width / 2, height / 2);
+
+            File destFile = new File(destName);
+            FileOutputStream out = new FileOutputStream(destFile);
+            int dotIndex = destName.lastIndexOf('.');
+            String format = destName.substring(dotIndex + 1);
+            ImageIO.write(resizedImage, format, out);
+            out.close();
+            System.out.println(imageName + " => " + destName);
+        } catch (Exception e) {}
+    }
+}
