9a6eca314fe7c34cbcfe31501645f1aa05d14acf
wxy325 <wxy325@qq.com>
2015-02-11 23:06:18 +0800

    Adjust goblin-tbitem

--- admin-tools/goblin-tbitem/lib/run.js
+++ admin-tools/goblin-tbitem/lib/run.js
@@ -20,23 +20,57 @@ process.on('uncaughtException', function(err) {
 });
 
 var appServerURL = argv['app-server-url'];
-
+appServerURL = "http://" + appServerURL + "/services"
 var _next = function(index) {
     var logs = [];
     async.waterfall([
-    function(callback) {
-        // TODO: Query item via app server
-        item = {};
+    function (callback) {
+        request.get({
+            'url' : appServerURL + '/goblin/queryItems',
+            'qs' : {
+                'pageNo' : index,
+                'pageSize' : 1
+            }
+        }, function (err, response, body) {
+            if (err) {
+                callback(err);
+                return;
+            }
+            var retObj = JSON.parse(body);
+            try {
+                callback(null, retObj.data.items[0]);
+            } catch (e) {
+                callback(e);
+            }
+        })
+    },
+    function(item, callback) {
         logs.push('item._id=' + item._id);
         callback(null, item.source);
     },
     function(source, callback) {
         // TODO Parse source
-        if (false) {
-            _taobao(source, callback);
+        /*
+        * http://detail.tmall.com/item.htm?spm=a1z10.4.w5003-9301197547.23.qjlW0g&id=40952601693&rn=2b5acab447292dedd6c7bc1d2e9a5387&abbucket=7&scene=taobao_shop
+        * http://item.taobao.com/item.htm?spm=2013.1.w5734072-1564537832.10.Vg0Rmz&id=42888614090
+        * */
+        var tmallRegex = /http:\/\/\w*\.tmall\.com/;
+        var taobaoRegex = /http:\/\/\w*\.taobao\.com/;
+        var idRegex = /id=(\d*)/
+        var idComp = source.match(idRegex);
+        var tbItemId = null;
+        if (idComp && idComp.length > 1) {
+            tbItemId = idComp[1];
         } else {
+            // TODO handle parse id error
+        }
+        if (source.match(tmallRegex)) {
             // TODO Implement tmall
-            _tmall(source, callback);
+            _tmall(tbItemId, callback);
+        } else if (source.match(taobaoRegex)) {
+            _taobao(tbItemId, callback);
+        } else {
+            // TODO handle error
         }
     },
     function(price, promoPrice, callback) {
@@ -66,11 +100,10 @@ var _next = function(index) {
         }
     });
 };
-_next(0);
+_next(1);
 
-var _taobao = function(source, callback) {
-    // TODO Parse tbItemId from source
-    var tbItemId = '41449399566';
+var _taobao = function(tbItemId, callback) {
+        var tbItemId = '41449399566';
     request.get({
         'url' : 'http://detailskip.taobao.com/json/sib.htm?p=1&itemId=' + tbItemId,
         'headers' : {
@@ -100,7 +133,6 @@ var _taobao = function(source, callback) {
 };
 
 var _tmall = function(tbItemId, callback) {
-    // TODO Parse tbItemId from source
     var tbItemId = '42550435119';
     request.get({
         'url' : 'http://mdskip.taobao.com/core/initItemDetail.htm?callback=setMdskip&itemId=' + tbItemId,
@@ -114,14 +146,17 @@ var _tmall = function(tbItemId, callback) {
             callback(err);
         } else {
             try {
+
                 var setMdskip = function(object) {
                     var price, promoPrice;
                     // TODO Parse g_config.PromoData to find the item.brandDiscountInfo.price
                     // TODO Parse g_config.Price to find the price
                     console.log(object.defaultModel.deliveryDO.deliveryAddress);
+                    //object.defaultModel.itemPriceResultDO.priceInfo
                     callback(null, price, promoPrice);
                 };
                 eval(new Iconv('gbk', 'utf-8').convert(new Buffer(body, 'binary')).toString());
+                console.log(g_config);
             } catch(err) {
                 callback(err);
             }
