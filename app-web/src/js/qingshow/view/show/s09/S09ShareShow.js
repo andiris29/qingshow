9601c438190d9e9bdf7e27211b6dc09047eba98e
andiris29 <andiris29@gmail.com>
2015-02-16 22:08:52 +0800

    Implement download

--- app-web/src/js/qingshow/view/show/s09/S09ShareShow.js
+++ app-web/src/js/qingshow/view/show/s09/S09ShareShow.js
@@ -1,14 +1,18 @@
 // @formatter:off
 define([
     'violet/utils/OOUtil',
+    'violet/utils/UADetector',
+    'violet/utils/InteractionUtil',
     'violet/ui/core/UIComponent',
     'qingshow/services/HTTPService',
     'qingshow/view/ViewFactory'
-], function(OOUtil, UIComponent, 
+], function(OOUtil, UADetector, InteractionUtil, UIComponent, 
     HTTPService, ViewFactory) {
 // @formatter:on
     var S09ShareShow = function(dom, options) {
         S09ShareShow.superclass.constructor.apply(this, arguments);
+
+        this._stageShow = null;
 
         async.parallel([
         function(callback) {
@@ -26,28 +30,40 @@ define([
                 'pageSize' : 2
             }, callback);
         }], function(err, results) {
-            var show = results[0].data.shows[0];
-            var thumbShows = results[1].data.shows.filter(function(thumbShow) {
-                return thumbShow._id !== show._id;
+            var stageMongoShow = results[0].data.shows[0];
+            var thumbMongoShows = results[1].data.shows.filter(function(thumbShow) {
+                return thumbShow._id !== stageMongoShow._id;
             });
             ViewFactory.create('/show/s09/Show', this.$('.qs-show').get(0), {
-                'show' : show
-            });
-            ViewFactory.create('/show/s09/ShowThumb', this.$('.qs-thumb-show:eq(0)').get(0), {
-                'show' : thumbShows[0]
-            });
-            ViewFactory.create('/show/s09/ShowThumb', this.$('.qs-thumb-show:eq(1)').get(0), {
-                'show' : thumbShows[1]
-            });
+                'show' : stageMongoShow
+            }, function(err, module) {
+                this._stageShow = module;
+            }.bind(this));
+            this._createThumbShow(this.$('.qs-thumb-show:eq(0)').get(0), thumbMongoShows[0]);
+            this._createThumbShow(this.$('.qs-thumb-show:eq(1)').get(0), thumbMongoShows[1]);
             ViewFactory.create('/show/s09/ItemThumb', this.$('.qs-thumb-item:eq(0)').get(0), {
                 'item' : results[2].data.items[0]
             });
             ViewFactory.create('/show/s09/ItemThumb', this.$('.qs-thumb-item:eq(1)').get(0), {
                 'item' : results[2].data.items[1]
             });
+        }.bind(this));
+        // Events for static stuff
+        InteractionUtil.onTouchOrClick(this.$('.qs-download'), function(event) {
+            if (UADetector.isAndroid()) {
+                // TODO Android
+            } else {
+                window.open('https://itunes.apple.com/us/app/qing-xiu/id946116105');
+            }
         });
     };
     OOUtil.extend(S09ShareShow, UIComponent);
+
+    S09ShareShow.prototype._createThumbShow = function(dom, mongoShow) {
+        ViewFactory.create('/show/s09/ShowThumb', dom, {
+            'show' : mongoShow
+        });
+    };
 
     return S09ShareShow;
 });
