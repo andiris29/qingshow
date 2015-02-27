05fd5701f38108ec0c39a27b111a653a7f96e2d6
Chenhr <rong5690001@gmail.com>
2015-02-27 16:32:18 +0800

    android-native:fix 调整U06，U07返回按钮大小

--- app-android-native/app/src/main/java/com/focosee/qingshow/activity/U06LoginActivity.java
+++ app-android-native/app/src/main/java/com/focosee/qingshow/activity/U06LoginActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -58,7 +59,7 @@ public class U06LoginActivity extends BaseActivity {
 
         requestQueue = Volley.newRequestQueue(context);
 
-        TextView backTextView = (TextView) findViewById(R.id.backTextView);
+        ImageView backTextView = (ImageView) findViewById(R.id.backImageView);
         backTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
