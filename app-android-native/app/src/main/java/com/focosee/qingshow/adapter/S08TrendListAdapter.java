859389a94f02912ede6edae9fd62cbc19de101c3
JiangDaYa0 <jzhaolong@gmail.com>
2015-03-02 11:11:16 +0800

    android-native:fix  #472

--- app-android-native/app/src/main/java/com/focosee/qingshow/adapter/S08TrendListAdapter.java
+++ app-android-native/app/src/main/java/com/focosee/qingshow/adapter/S08TrendListAdapter.java
@@ -133,36 +133,11 @@ public class S08TrendListAdapter extends BaseAdapter {
         holderView.viewPager.setOnPageChangeListener(mViewPagerAdapter);
         //设置ViewPager的默认项, 设置为长度的100倍，这样子开始就能往左滑动
         holderView.viewPager.setOffscreenPageLimit(1);
-        holderView.viewPager.setCurrentItem(data.get(position).images.size() * 100);
+        holderView.viewPager.setCurrentItem(data.get(position).images.size() * 3);
         //分享
         holderView.shareImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                ShareSDK.initSDK(context);
-//                OnekeyShare oks = new OnekeyShare();
-//                //关闭sso授权
-//                oks.disableSSOWhenAuthorize();
-//
-//// 分享时Notification的图标和文字
-//                oks.setNotification(R.drawable.app_icon, context.getString(R.string.app_name));
-//                // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
-//                oks.setTitle(context.getString(R.string.share));
-//                // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
-//                oks.setTitleUrl("http://sharesdk.cn");
-//                // text是分享文本，所有平台都需要这个字段
-//                oks.setText("欢迎大家过来使用~");
-//                // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
-////                oks.setImagePath(getResources().getResourceName(R.drawable.app_icon));//确保SDcard下面存在此张图片
-//                // url仅在微信（包括好友和朋友圈）中使用
-//                oks.setUrl("http://sharesdk.cn");
-//                // comment是我对这条分享的评论，仅在人人网和QQ空间使用
-//                oks.setComment("快来试用吧~");
-//                // site是分享此内容的网站名称，仅在QQ空间使用
-//                oks.setSite(context.getString(R.string.app_name));
-//                // siteUrl是分享此内容的网站地址，仅在QQ空间使用
-//                oks.setSiteUrl("http://sharesdk.cn");
-//// 启动分享GUI
-//                oks.show(context);
 
             }
         });
