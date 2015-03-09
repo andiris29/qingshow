bbd7b2141acaaf3c61d9f10bc9ecf8c422aefaec
wxy325 <wxy325@qq.com>
2015-03-10 00:14:42 +0800

    fix #501, move admin-tools/goblin-item to server-node/scheduled/goblin-item, add schedule startup

---
+++ server-node/lib/httpserver/scheduled/goblin/goblin-item.js
@@ -0,0 +1,96 @@
+var goblinItem =  module.exports;
+
+var async = require('async');
+var winston = require('winston');
+var _ = require('underscore');
+
+var Item = require('../../../model/items');
+var MongoHelper = require('../../helpers/MongoHelper');
+var taobaoMongoHelper = require('../../taobao/taobaoMongoHelper');
+var ServerError = require('../../server-error');
+
+
+goblinItem.start = function (startDate) {
+    _next(startDate, 10);
+};
+
+
+var _next = function (time, retryTime) {
+    async.waterfall([
+        function (callback) {
+            var criteria = {
+                '$and': [{
+                    '$or': [{
+                        'taobaoInfo.refreshTime': {
+                            '$exists': false
+                        }
+                    }, {
+                        'taobaoInfo.refreshTime': {
+                            '$lt': time
+                        }
+                    }]
+                }, {
+                    '$or': [{
+                        'deactive': {
+                            '$exists': false
+                        }
+                    }, {
+                        'deactive': false
+                    }]
+                }]
+            };
+
+            var query = Item.find(criteria);
+            var queryCount = Item.find(criteria);
+            var pageNo = 1;
+            var pageSize = 10;
+            MongoHelper.queryPaging(query, queryCount, pageNo, pageSize, function (err, result, count) {
+                winston.info('goblin item : remain item ' + count);
+                if (err) {
+                    callback(err);
+                } else if (!count) {
+                    callback(ServerError.PagingNotExist);
+                } else {
+                    callback(null, result);
+                }
+            });
+        },
+        function (result, callback) {
+            items = result;
+            var tasks = [];
+            items.forEach(function (item) {
+                var task = function (callback) {
+                    taobaoMongoHelper.crawlItemTaobaoInfo(item, callback);
+                };
+                tasks.push(task);
+            });
+            async.parallel(tasks, callback);
+
+        }
+    ], function (err) {
+        //TODO
+        if (err === ServerError.PagingNotExist) {
+            //finish
+            winston.info('goblin item daily complete');
+            return;
+        }
+
+        if (err) {
+            winston.info('gobin item error:');
+            winston.info(err);
+            retryTime -= 1;
+            if (retryTime > 0) {
+                winston.info('remain retry : ' + retryTime);
+                _.delay(function() {
+                    _next(time, retryTime);
+                }, 1000);
+            } else {
+                winston.info('Error: goblin item retry time is zero, stop.');
+            }
+        } else {
+            _.delay(function () {
+                _next(time, retryTime);
+            }, 1000);
+        }
+    });
+};
--- server-node/lib/httpserver/taobao/taobaoUtil.js
+++ server-node/lib/httpserver/taobao/taobaoUtil.js
@@ -1,10 +1,10 @@
-var helper = module.exports;
+var util = module.exports;
 
 /*
  * http://detail.tmall.com/item.htm?spm=a1z10.4.w5003-9301197547.23.qjlW0g&id=40952601693&rn=2b5acab447292dedd6c7bc1d2e9a5387&abbucket=7&scene=taobao_shop
  * http://item.taobao.com/item.htm?spm=2013.1.w5734072-1564537832.10.Vg0Rmz&id=42888614090
  * */
-helper.getIidFromSource = function (source) {
+util.getIidFromSource = function (source) {
     var idRegex = /id=(\d*)/;
     var idComp = source.match(idRegex);
     if (idComp && idComp.length > 1) {
@@ -14,12 +14,12 @@ helper.getIidFromSource = function (source) {
     }
 };
 
-helper.isFromTmall = function (source) {
+util.isFromTmall = function (source) {
     var tmallRegex = /http:\/\/\w*\.tmall\.com/;
     return source.match(tmallRegex);
 };
 
-helper.isFromTaobao = function (source) {
+util.isFromTaobao = function (source) {
     var taobaoRegex = /http:\/\/\w*\.taobao\.com/;
     return source.match(taobaoRegex );
 };
