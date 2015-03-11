5ea8ab6995c5718898d0de2e029b608a7c7aa93c
wxy325 <wxy325@qq.com>
2015-03-12 00:53:00 +0800

    ios: order list cell

--- app-ios-native/qingshow-ios-native/vc/user/setting/view/order/QSOrderListTableViewCell.h
+++ app-ios-native/qingshow-ios-native/vc/user/setting/view/order/QSOrderListTableViewCell.h
@@ -8,6 +8,48 @@
 
 #import <UIKit/UIKit.h>
 
+@class QSOrderListTableViewCell;
+
+@protocol QSOrderListTableViewCellDelegate <NSObject>
+
+- (void)didClickRefundBtnForCell:(QSOrderListTableViewCell*)cell;
+- (void)didClickLogisticBtnForCell:(QSOrderListTableViewCell*)cell;
+- (void)didClickSubmitBtnForCell:(QSOrderListTableViewCell*)cell;
+
+@end
+
+typedef NS_ENUM(NSUInteger, QSOrderListTableViewCellType) {
+    QSOrderListTableViewCellTypeComplete,
+    QSOrderListTableViewCellTypeWaiting
+};
+
 @interface QSOrderListTableViewCell : UITableViewCell
 
+@property (weak, nonatomic) IBOutlet UILabel* orderIdLabel;
+@property (weak, nonatomic) IBOutlet UILabel* stateLabel;
+@property (weak, nonatomic) IBOutlet UILabel* titleLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel* sizeLabel;
+@property (weak, nonatomic) IBOutlet UILabel* quantityLabel;
+@property (weak, nonatomic) IBOutlet UILabel* colorLabel;
+@property (weak, nonatomic) IBOutlet UILabel* priceLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel* dateStartTextLabel;
+@property (weak, nonatomic) IBOutlet UILabel* dateStartLabel;
+@property (weak, nonatomic) IBOutlet UILabel* dateEndTextLabel;
+@property (weak, nonatomic) IBOutlet UILabel* dateEndLabel;
+
+@property (weak, nonatomic) IBOutlet UIButton* refundButton;
+@property (weak, nonatomic) IBOutlet UIButton* logisticButton;
+@property (weak, nonatomic) IBOutlet UIButton* submitButton;
+
+@property (assign, nonatomic) QSOrderListTableViewCellType type;
+
+- (IBAction)refundBtnPressed:(id)sender;
+- (IBAction)logisticBtnPressed:(id)sender;
+- (IBAction)submitBtnPressed:(id)sender;
+
+- (void)bindWithDict:(NSDictionary*)dict;
+
+@property (weak, nonatomic) NSObject<QSOrderListTableViewCellDelegate>* delegate;
 @end
--- app-ios-native/qingshow-ios-native/vc/user/setting/view/order/QSOrderListTableViewCell.m
+++ app-ios-native/qingshow-ios-native/vc/user/setting/view/order/QSOrderListTableViewCell.m
@@ -7,11 +7,24 @@
 //
 
 #import "QSOrderListTableViewCell.h"
+#import <QuartzCore/QuartzCore.h>
 
 @implementation QSOrderListTableViewCell
 
+#pragma mark - Init Config
 - (void)awakeFromNib {
     // Initialization code
+    [self configBtn:self.refundButton];
+    [self configBtn:self.logisticButton];
+    [self configBtn:self.submitButton];
+}
+- (void)configBtn:(UIButton*)btn
+{
+    UIColor* color = btn.titleLabel.textColor;
+    btn.layer.borderColor = color.CGColor;
+    btn.layer.borderWidth = 1.f;
+    btn.layer.cornerRadius = 4.f;
+    btn.layer.masksToBounds = YES;
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
@@ -20,4 +33,60 @@
     // Configure the view for the selected state
 }
 
+#pragma mark - Binding
+- (void)bindWithDict:(NSDictionary*)dict
+{
+}
+#pragma mark - Getter And Setter
+- (void)setType:(QSOrderListTableViewCellType)type
+{
+    _type = type;
+    BOOL fHiddenLabel = NO;
+    switch (type) {
+        case QSOrderListTableViewCellTypeComplete:
+        {
+            self.stateLabel.text = @"交易完成";
+            fHiddenLabel = NO;
+
+            break;
+        }
+        case QSOrderListTableViewCellTypeWaiting:
+        {
+            self.stateLabel.text = @"待收货";
+            fHiddenLabel = YES;
+            break;
+        }
+        default:
+        {
+            break;
+        }
+    }
+    self.dateEndLabel.hidden = fHiddenLabel;
+    self.dateEndTextLabel.hidden = fHiddenLabel;
+    self.dateStartLabel.hidden = fHiddenLabel;
+    self.dateStartTextLabel.hidden = fHiddenLabel;
+    self.submitButton.hidden = !fHiddenLabel;
+    self.refundButton.hidden = !fHiddenLabel;
+    self.logisticButton.hidden = !fHiddenLabel;
+}
+
+#pragma mark - IBAction
+- (IBAction)refundBtnPressed:(id)sender
+{
+    if ([self.delegate respondsToSelector:@selector(didClickRefundBtnForCell:)]) {
+        [self.delegate didClickRefundBtnForCell:self];
+    }
+}
+- (IBAction)logisticBtnPressed:(id)sender
+{
+    if ([self.delegate respondsToSelector:@selector(didClickLogisticBtnForCell:)]) {
+        [self.delegate didClickLogisticBtnForCell:self];
+    }
+}
+- (IBAction)submitBtnPressed:(id)sender
+{
+    if ([self.delegate respondsToSelector:@selector(didClickSubmitBtnForCell:)]) {
+        [self.delegate didClickSubmitBtnForCell:self];
+    }
+}
 @end
