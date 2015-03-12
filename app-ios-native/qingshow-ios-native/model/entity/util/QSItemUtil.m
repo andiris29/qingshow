4c58cc62ec2c24cdd2f7b50d2904ca7bc1d9ca90
andiris29 <andiris29@gmail.com>
2015-03-12 13:47:22 +0800

    Fix price display

--- app-ios-native/qingshow-ios-native/model/entity/util/QSItemUtil.m
+++ app-ios-native/qingshow-ios-native/model/entity/util/QSItemUtil.m
@@ -246,7 +246,7 @@
     NSDictionary* maxSku = [sortedSkus lastObject];
     NSNumber* maxPrice = maxSku[@"price"];
     if ([self hasDiscountInfo:itemDict]) {
-        return [NSString stringWithFormat:@"￥%.2f-%.2f", minPrice.doubleValue, maxPrice.doubleValue];
+//        return [NSString stringWithFormat:@"￥%.2f-%.2f", minPrice.doubleValue, maxPrice.doubleValue];
         //min(skus[i].price) + ' - ' + max(skus[i].price)
         if (sortedSkus.count == 1 || (ABS(maxPrice.doubleValue - minPrice.doubleValue)) < 0.01) {
             return [NSString stringWithFormat:@"￥%.2f", (minPrice.doubleValue - 0.01)];
