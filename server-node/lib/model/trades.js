ac95794bc116feb0bd57a056ac334588a3332e37
HashMap <sebastianqu@gmail.com>
2015-03-13 16:38:45 +0800

    add module trades

---
+++ server-node/lib/model/trades.js
@@ -0,0 +1,57 @@
+var Item = require('../../model/items');
+var People = require('../../model/peoples');
+var tradeSchema;
+tradeSchema = Schema({
+    status : Number,
+    price : Number,
+    orders : [{
+        quantity : Number,
+        price : Number,
+        itemSnapshot : Item,
+        peopleSnapshot : People,
+        r : {
+            skuIndex: Number,
+            receiverIndex : Number
+        }
+    }],
+    pay : {
+        weixin : {
+            appid : String,
+            partnerid : String,
+            prepayid : String
+        },
+        alipay : {
+            partner : String,
+            seller : String
+        }
+    },
+    taobaoInfo : {
+        userNick : String,
+        tradeID : String
+    },
+    logistic : {
+        company : String,
+        trackingID : String
+    },
+    'return' : {
+        logistic : {
+            company : String,
+            trackingID : String
+        }
+    },
+    create : {
+        type : Date,
+        'default' : Date.now
+    },
+    statusLogs : [{
+        status : Number,
+        update : {
+            type : Date,
+            'default' : Date.now
+        },
+        peopleRef : {
+            type : Schema.Types.ObjectId,
+            ref : 'peoples';
+        }
+    }]
+});
