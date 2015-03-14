6b56b5380401c45849c71f4632673783bd547365
wxy325 <wxy325@qq.com>
2015-03-14 16:13:23 +0800

    ios: refund view controller

---
+++ app-ios-native/qingshow-ios-native/vc/user/setting/QSU12RefundViewController.h
@@ -0,0 +1,15 @@
+//
+//  QSU12RefundViewController.h
+//  qingshow-ios-native
+//
+//  Created by wxy325 on 3/14/15.
+//  Copyright (c) 2015 QS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QSU12RefundViewController : UIViewController
+
+- (instancetype)initWithDict:(NSDictionary*)orderDict;
+
+@end
---
+++ app-ios-native/qingshow-ios-native/vc/user/setting/QSU12RefundViewController.m
@@ -0,0 +1,44 @@
+//
+//  QSU12RefundViewController.m
+//  qingshow-ios-native
+//
+//  Created by wxy325 on 3/14/15.
+//  Copyright (c) 2015 QS. All rights reserved.
+//
+
+#import "QSU12RefundViewController.h"
+
+@interface QSU12RefundViewController ()
+
+@property (strong, nonatomic) NSDictionary* orderDict;
+@end
+
+@implementation QSU12RefundViewController
+
+#pragma mark - Init
+- (instancetype)initWithDict:(NSDictionary*)orderDict
+{
+    self = [super initWithNibName:@"QSU12RefundViewController" bundle:nil];
+    
+    if (self) {
+        self.orderDict = orderDict;
+    }
+    
+    return self;
+}
+
+#pragma mark - Life Cycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
