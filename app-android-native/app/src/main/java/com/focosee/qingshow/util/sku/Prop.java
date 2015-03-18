c4eca5e5f88bb55da664d1b954c98d0f6303e093
JiangDaYa0 <jzhaolong@gmail.com>
2015-03-18 11:50:15 +0800

    android-native:fix SKU prop Util

--- app-android-native/app/src/main/java/com/focosee/qingshow/util/sku/Prop.java
+++ app-android-native/app/src/main/java/com/focosee/qingshow/util/sku/Prop.java
@@ -4,4 +4,54 @@ package com.focosee.qingshow.util.sku;
  * Created by Administrator on 2015/3/18.
  */
 public class Prop {
+    String propId;
+    String propValue;
+    String name;
+
+    public String getPropId() {
+        return propId;
+    }
+
+    public void setPropId(String propId) {
+        this.propId = propId;
+    }
+
+    public String getPropValue() {
+        return propValue;
+    }
+
+    public void setPropValue(String propValue) {
+        this.propValue = propValue;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Prop prop = (Prop) o;
+
+        if (name != null ? !name.equals(prop.name) : prop.name != null) return false;
+        if (propId != null ? !propId.equals(prop.propId) : prop.propId != null) return false;
+        if (propValue != null ? !propValue.equals(prop.propValue) : prop.propValue != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = propId != null ? propId.hashCode() : 0;
+        result = 31 * result + (propValue != null ? propValue.hashCode() : 0);
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
 }
--- app-android-native/app/src/main/java/com/focosee/qingshow/util/sku/SkuUtil.java
+++ app-android-native/app/src/main/java/com/focosee/qingshow/util/sku/SkuUtil.java
@@ -1,7 +1,66 @@
 package com.focosee.qingshow.util.sku;
 
+import android.text.TextUtils;
+
+import com.focosee.qingshow.model.vo.mongo.MongoItem;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+
 /**
  * Created by Administrator on 2015/3/18.
  */
 public class SkuUtil {
+
+    public enum KEY {
+
+        COLOR("color", "1627207"),
+        SIZE_1("size", "20509"),
+        SIZE_2("size", "20518"),
+        SIZE_3("size", "20549");
+
+        public String name;
+        public String id;
+
+        KEY(String name, String id) {
+            this.name = name;
+            this.id = id;
+        }
+    }
+
+    public static HashMap<ArrayList<Prop>, MongoItem.TaoBaoInfo.SKU> filter(LinkedList<MongoItem.TaoBaoInfo.SKU> skus) {
+
+        HashMap<ArrayList<Prop>, MongoItem.TaoBaoInfo.SKU> skusProp;
+        if (null == skus) {
+            return null;
+        }
+        skusProp = new HashMap<ArrayList<Prop>, MongoItem.TaoBaoInfo.SKU>();
+        for (MongoItem.TaoBaoInfo.SKU sku : skus) {
+
+            ArrayList<Prop> props = new ArrayList<Prop>();
+            for (int i = 0; i < sku.properties_name.split(";").length; i++) {
+
+                Prop prop = new Prop();
+
+                String propStr = sku.properties.split(";")[i + 1];
+                if (!TextUtils.isEmpty(propStr)) {
+                    String strs[] = propStr.split(":");
+                    String propId = strs[0];
+                    String propValue = strs[1];
+                    prop.setPropId(propId);
+                    prop.setPropValue(propValue);
+                }
+
+                String name = sku.properties_name.split(";")[i];
+                prop.setName(name);
+
+                props.add(prop);
+            }
+
+            skusProp.put(props, sku);
+        }
+
+        return skusProp;
+    }
 }
