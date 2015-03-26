8317fb3c9b3c130e9dd749173bd9880e6a4c2560
wxy325 <wxy325@qq.com>
2015-03-26 11:23:53 +0800

    ios : impl u12

--- app-ios-native/qingshow-ios-native/vc/user/setting/QSU12RefundViewController.m
+++ app-ios-native/qingshow-ios-native/vc/user/setting/QSU12RefundViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "QSU12RefundViewController.h"
+#import <QuartzCore/QuartzCore.h>
+#import "QSDateUtil.h"
 
 @interface QSU12RefundViewController ()
 
@@ -32,20 +34,120 @@
 }
 
 #pragma mark - Life Cycle
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     self.title = @"退货方式";
     self.widthCon.constant = [UIScreen mainScreen].bounds.size.width;
 //    ((UIScrollView*)self.view).contentInset = UIEdgeInsetsMake(0, 0, 300.f, 0);
+    UITapGestureRecognizer* ges = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(didTapView)];
+    [self.scrollVIew addGestureRecognizer:ges];
+    [self setCurrentSelectedDate:[NSDate date]];
+    
+    UITapGestureRecognizer* tapDate = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(didTapTextField)];
+    [self.sendDateTextField addGestureRecognizer:tapDate];
+}
+- (void)didTapTextField
+{
+    [self showPicker];
+    
+}
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillShow:)
+                                                 name:UIKeyboardWillShowNotification
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillHide:)
+                                                 name:UIKeyboardWillHideNotification
+                                               object:nil];
+}
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+#pragma mark - Gesture
+- (void)didTapView
+{
+    [self hideKeyboardAndPicker];
+}
+- (void)hideKeyboardAndPicker
+{
+    for (UITextField* f in @[self.companyTextField, self.expressOrderTextField, self.sendDateTextField]) {
+        [f resignFirstResponder];
+    }
+    [self hidePicker];
+    [UIView animateWithDuration:0.5f animations:^{
+        self.scrollVIew.contentInset = UIEdgeInsetsZero;
+    }];
 
+}
+#pragma mark - Keyboard
+- (void)configContentInset:(float)height
+{
+    self.scrollVIew.contentInset = UIEdgeInsetsMake(0, 0, height, 0);
+    [self scrollToBottom:height];
+}
+- (void)keyboardWillShow:(NSNotification *)notif {
+    [self configContentInset:300.f];
+}
 
+- (void)keyboardWillHide:(NSNotification *)notif {
+    [UIView animateWithDuration:0.5f animations:^{
+        self.scrollVIew.contentInset = UIEdgeInsetsZero;
+    }];
+}
 
+- (void)scrollToBottom:(float)keyboardHeight
+{
+    [self.scrollVIew setContentOffset:CGPointMake(0, keyboardHeight) animated:YES];
+}
+
+- (IBAction)submitBtnPressed:(id)sender {
+    [self hideKeyboardAndPicker];
+}
+
+#pragma mark - Picker
+
+- (IBAction)pickerValueChanged:(UIDatePicker *)sender {
+    NSDate* date = sender.date;
+    [self setCurrentSelectedDate:date];
+}
+- (void)showPicker
+{
+    if (!self.datePicker.hidden) {
+        return;
+    }
+    CATransition* tran = [[CATransition alloc] init];
+    tran.type = kCATransitionPush;
+    tran.subtype = kCATransitionFromTop;
+    [self.datePicker.layer addAnimation:tran forKey:@"show"];
+    self.datePicker.hidden = NO;
+    [self configContentInset:100];
+}
+- (void)hidePicker
+{
+    if (self.datePicker.hidden) {
+        return;
+    }
+    CATransition* tran = [[CATransition alloc] init];
+    tran.type = kCATransitionPush;
+    tran.subtype = kCATransitionFromBottom;
+    [self.datePicker.layer addAnimation:tran forKey:@"hide"];
+    self.datePicker.hidden = YES;
+    
+}
+- (void)setCurrentSelectedDate:(NSDate*)date
+{
+    self.sendDateTextField.text = [QSDateUtil buildDateStringFromDate:date];
+}
 @end
