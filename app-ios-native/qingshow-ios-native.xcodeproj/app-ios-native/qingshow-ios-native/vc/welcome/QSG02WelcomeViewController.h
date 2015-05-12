a9b302b292cc74fb3b0d92ea672731707b9edbbc
mhy <mhy15287570@163.com>
2015-05-12 14:48:33 +0800

    welcomeVC refresh 2

--- app-ios-native/qingshow-ios-native.xcodeproj/app-ios-native/qingshow-ios-native/vc/welcome/QSG02WelcomeViewController.h
+++ app-ios-native/qingshow-ios-native.xcodeproj/app-ios-native/qingshow-ios-native/vc/welcome/QSG02WelcomeViewController.h
@@ -16,7 +16,7 @@
 
 @end
 
-@interface QSG02WelcomeViewController : UIViewController<UIScrollViewDelegate>
+@interface QSG02WelcomeViewController : UIViewController
 @property (weak, nonatomic) IBOutlet UIButton *loginBtn;
 
 @property (weak, nonatomic) IBOutlet UIScrollView *welcomeSCV;
