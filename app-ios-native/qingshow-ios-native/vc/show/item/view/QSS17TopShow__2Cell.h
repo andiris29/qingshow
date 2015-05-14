dd336cda648305137530a6b4443041ac3cb180d6
doupei <431581442@qq.com>
2015-05-14 11:39:44 +0800

    iOS－native CellChange

---
+++ app-ios-native/qingshow-ios-native/vc/show/item/view/QSS17TopShow__2Cell.h
@@ -0,0 +1,17 @@
+//
+//  QSS17TopShow_2Cell.h
+//  qingshow-ios-native
+//
+//  Created by Han Hugh on 15/5/14.
+//  Copyright (c) 2015年 QS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#define TOPSHOW_2INDENTIFIER @"TopShow_2Cell"
+
+@interface QSS17TopShow__2Cell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *backImage;
+@property (weak, nonatomic) IBOutlet UIButton *likeButton;
+@property (weak, nonatomic) IBOutlet UIView *backView;
+
+@end
