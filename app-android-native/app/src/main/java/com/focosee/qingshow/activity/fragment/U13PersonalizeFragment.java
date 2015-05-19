041c3ca8abcbf2035134399f1e156bc7da9a9cd8
DylanJiang <jzhaolong@gmail.com>
2015-05-19 18:20:25 +0800

    android-native:update u13

--- app-android-native/app/src/main/java/com/focosee/qingshow/activity/fragment/U13PersonalizeFragment.java
+++ app-android-native/app/src/main/java/com/focosee/qingshow/activity/fragment/U13PersonalizeFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -12,10 +13,12 @@ import com.focosee.qingshow.R;
 import com.focosee.qingshow.widget.radio.MyRadioGroup;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.InjectViews;
+import butterknife.OnClick;
 
 /**
  * Created by DylanJiang on 15/5/6.
@@ -36,17 +39,32 @@ public class U13PersonalizeFragment extends Fragment {
     @InjectView(R.id.weight_text)
     TextView weightText;
 
-    @InjectView(R.id.body_style)
-    MyRadioGroup bodyStyle;
+    @InjectViews({R.id.body_a,R.id.body_h,R.id.body_v,R.id.body_x})
+    List<RadioButton> bodys;
+
+    private int checkBodyNum;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_u13_personalize, container, false);
         ButterKnife.inject(this, view);
-        initSeekBar(age,ageText,1,100);
+        initSeekBar(age, ageText, 1, 100);
         initSeekBar(height,heightText,150,50);
-        initSeekBar(weight,weightText,30,70);
+        initSeekBar(weight, weightText, 30, 70);
         return view;
+    }
+
+    @OnClick({R.id.body_a,R.id.body_h,R.id.body_v,R.id.body_x})
+    public void initBodyStyle(RadioButton radio) {
+        for (int i = 0; i < bodys.size(); i++) {
+            RadioButton body = bodys.get(i);
+            if (radio.getId() == body.getId()){
+                checkBodyNum = i;
+                body.setChecked(true);
+            }else {
+                body.setChecked(false);
+            }
+        }
     }
 
     private void initSeekBar(SeekBar bar,final TextView text, final int startNum,int copies) {
