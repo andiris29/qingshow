d365ce89277df59cb4b47d3adf34b8d836f48013
liushaoyi <mailshaoyi@163.com>
2015-05-22 15:49:46 +0800

    add favoInfoModel and  perfect QSU14VC

--- app-ios-native/qingshow-ios-native/vc/favorite/Controller/QSU14FavoriteViewController.m
+++ app-ios-native/qingshow-ios-native/vc/favorite/Controller/QSU14FavoriteViewController.m
@@ -8,10 +8,17 @@
 
 #import "QSU14FavoriteViewController.h"
 #import "QSU14DisplayCell.h"
+#import "QSNetworkKit.h"
+#import "QSShowUtil.h"
+#import "QSFavoInfo.h"
 #import "Common.h"
 
 @interface QSU14FavoriteViewController ()
+
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+//tableView 数据源
+@property (nonatomic,strong)NSMutableArray *dataArray;
 
 @end
 
@@ -27,12 +34,32 @@
     //注册cell
     [self.tableView registerNib:[UINib nibWithNibName:@"QSU14DisplayCell" bundle:[NSBundle mainBundle]] forCellReuseIdentifier:@"displayCell"];
     
+    //获取数据源
+    [self loadFavotiteData];
+    
+}
+
+#pragma mark --获取网络数据
+- (void)loadFavotiteData
+{
+        [SHARE_NW_ENGINE getTestShowsOnSucceed:^(NSArray *array, NSDictionary *metadata) {
+//            NSLog(@"array = %@,dic = %@",array,metadata);
+            //数据转模型
+            NSArray *favoInfoArray = [QSFavoInfo favoInfoWithDicArray:array];
+            
+            //模型传递
+            _dataArray = [NSMutableArray arrayWithArray:favoInfoArray];
+            
+            [self.tableView reloadData];
+        } onError:^(NSError *error) {
+            NSLog(@"TopShow Page  NetWorlk Error!");
+        }];
 }
 
 #pragma mark //UITableViewDataSource
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 10;
+    return _dataArray.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -41,7 +68,8 @@
     if (cell == nil) {
         cell = [[QSU14DisplayCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"displayCell"];
     }
-    
+    QSFavoInfo *favoInfo = _dataArray[indexPath.row];
+    [cell setValueForSubViewsWith:favoInfo];
     return cell;
 }
 
@@ -55,5 +83,5 @@
         return 215;
     }
 }
-#pragma mark //UITableViewDelegate
+#pragma mark --UITableViewDelegate
 @end
--- app-ios-native/qingshow-ios-native/vc/favorite/View/QSU14DisplayCell.h
+++ app-ios-native/qingshow-ios-native/vc/favorite/View/QSU14DisplayCell.h
@@ -8,6 +8,35 @@
 
 #import <UIKit/UIKit.h>
 
+@class QSFavoInfo;
+
 @interface QSU14DisplayCell : UITableViewCell
 
+//cell的背景图片
+@property (weak, nonatomic) IBOutlet UIImageView *backImage;
+
+//套装图片
+@property (weak, nonatomic) IBOutlet UIImageView *suitImageView;
+//单品图片
+@property (weak, nonatomic) IBOutlet UIImageView *skuImageView1;
+
+@property (weak, nonatomic) IBOutlet UIImageView *skuImageView2;
+
+@property (weak, nonatomic) IBOutlet UIImageView *skuImageView3;
+
+@property (weak, nonatomic) IBOutlet UIImageView *skuImageView4;
+
+//相应控件
+@property (weak, nonatomic) IBOutlet UIButton *suitButton;
+
+@property (weak, nonatomic) IBOutlet UIButton *skuButton1;
+
+@property (weak, nonatomic) IBOutlet UIButton *skuButton2;
+
+@property (weak, nonatomic) IBOutlet UIButton *skuButton3;
+
+@property (weak, nonatomic) IBOutlet UIButton *skuButton4;
+
+//cell赋值
+- (void)setValueForSubViewsWith:(QSFavoInfo *)favoInfo;
 @end
