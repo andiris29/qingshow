19849009bac9cd1e553c940d404d804c90c852f1
DylanJiang <jzhaolong@gmail.com>
2015-05-24 12:24:34 +0800

    android-native:add navigation

--- app-android-native/app/src/main/java/com/focosee/qingshow/activity/U01UserActivity.java
+++ app-android-native/app/src/main/java/com/focosee/qingshow/activity/U01UserActivity.java
@@ -1,13 +1,31 @@
 package com.focosee.qingshow.activity;
 
+import android.annotation.TargetApi;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.focosee.qingshow.R;
 import com.focosee.qingshow.adapter.U01PushAdapter;
 import com.focosee.qingshow.model.vo.mongo.Bean;
+import com.focosee.qingshow.util.BitMapUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,13 +34,27 @@ import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 public class U01UserActivity extends BaseActivity {
+
     @InjectView(R.id.recycler)
     RecyclerView recyclerView;
     @InjectView(R.id.user_bg)
     ImageView userBg;
 
+    @InjectView(R.id.drawer)
+    DrawerLayout drawer;
+    @InjectView(R.id.navigation)
+    LinearLayout navigation;
+    @InjectView(R.id.blur)
+    ImageView blur;
+    @InjectView(R.id.context)
+    RelativeLayout right;
+
+    private final float DAMP = 3.0f;
+
     private List<Bean> list;
-    private final float DAMP = 5.0f;
+    private U01PushAdapter adapter;
+
+    private boolean isFirstFocus = true;
 
     public void reconn() {
 
@@ -34,11 +66,91 @@ public class U01UserActivity extends BaseActivity {
         setContentView(R.layout.activity_u01_base);
         ButterKnife.inject(this);
         initData();
-        initView();
+        initDrawer();
+        initRectcler();
+    }
+
+    private void initDrawer() {
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawer,
+                R.string.menu_open, R.string.menu_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                openMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                closeMenu();
+            }
+
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                if (slideOffset == 0.0) blur.setVisibility(View.INVISIBLE);
+            }
+        };
+        drawer.setDrawerListener(drawerToggle);
+    }
+
+    private void closeMenu() {
+        blur.setVisibility(View.INVISIBLE);
+        drawer.closeDrawer(navigation);
+    }
+
+    private boolean isMenuOpened() {
+        return drawer.isDrawerOpen(navigation);
+    }
+
+    private void openMenu() {
+
+        if (isFirstFocus) {
+            applyBlur();
+            if (Build.VERSION.SDK_INT > 16)
+                isFirstFocus = true;
+        }
+        blur.setVisibility(View.VISIBLE);
+
+        navigation.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return true;
+            }
+        });
+        drawer.openDrawer(navigation);
+    }
+
+    private void blur(Bitmap bkg) {
+        Handler mHandler = new Handler() {
+            @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+            @Override
+            public void handleMessage(Message msg) {
+                blur.setBackground(new BitmapDrawable(U01UserActivity.this.getResources(),
+                        (Bitmap) msg.obj));
+                right.destroyDrawingCache();
+            }
+        };
+        Bitmap overlay = BitMapUtil.convertToBlur(bkg, this);
+        Message msg = mHandler.obtainMessage(1, 1, 1, overlay);
+        mHandler.sendMessage(msg);
+    }
+
+    private void applyBlur() {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                right.setDrawingCacheEnabled(true);
+                right.buildDrawingCache();
+                Bitmap bitmap = right.getDrawingCache();
+
+                blur(bitmap);
+            }
+        };
+        thread.run();
     }
 
 
-    private void initView() {
+    private void initRectcler() {
+        adapter = new U01PushAdapter(list, this,
+                R.layout.item_u01_push, R.layout.item_u01_header);
         GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
         layoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
             @Override
@@ -50,19 +162,16 @@ public class U01UserActivity extends BaseActivity {
             }
         });
         recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(new U01PushAdapter(list, this,
-                R.layout.item_u01_push, R.layout.item_u01_header));
+        recyclerView.setAdapter(adapter);
 
         recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
             public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
                 super.onScrolled(recyclerView, dx, dy);
                 float offset = recyclerView.computeVerticalScrollOffset();
-                userBg.setY(- offset / DAMP);
+                userBg.setY(-offset / DAMP);
             }
         });
-
-
     }
 
     private void initData() {
@@ -84,6 +193,22 @@ public class U01UserActivity extends BaseActivity {
         }
     }
 
+    @Override
+    public void onBackPressed() {
+        if (drawer.isScrollbarFadingEnabled() && isMenuOpened())
+            closeMenu();
+        else
+            super.onBackPressed();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_MENU) {
+            if (isMenuOpened()) closeMenu();
+            else openMenu();
+        }
+        return true;
+    }
 
     @Override
     protected void onDestroy() {
