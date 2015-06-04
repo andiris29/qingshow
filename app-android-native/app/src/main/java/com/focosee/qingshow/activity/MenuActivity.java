a90f1d5fe4f1e788340d22f15a977a8f339c4578
rong5690001 <rong5690001@gmail.com>
2015-06-04 21:14:19 +0800

    android-native:add MenuActivity

---
+++ app-android-native/app/src/main/java/com/focosee/qingshow/activity/MenuActivity.java
@@ -0,0 +1,167 @@
+package com.focosee.qingshow.activity;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.widget.DrawerLayout;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import com.focosee.qingshow.R;
+import com.focosee.qingshow.model.QSModel;
+import com.focosee.qingshow.util.BitMapUtil;
+import butterknife.InjectView;
+
+public class MenuActivity extends BaseActivity implements View.OnClickListener{
+
+    @InjectView(R.id.drawer)
+    DrawerLayout drawer;
+    @InjectView(R.id.navigation)
+    LinearLayout navigation;
+    @InjectView(R.id.blur)
+    ImageView blur;
+    @InjectView(R.id.context)
+    LinearLayout right;
+    @InjectView(R.id.s17_settting)
+    ImageView settingBtn;
+
+    private boolean isFirstFocus = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    private void initDrawer() {
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawer,
+                R.string.menu_open, R.string.menu_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                openMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                closeMenu();
+            }
+
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                if (slideOffset == 0.0) blur.setVisibility(View.INVISIBLE);
+            }
+        };
+        drawer.setDrawerListener(drawerToggle);
+    }
+
+    private void closeMenu() {
+        blur.setVisibility(View.INVISIBLE);
+        drawer.closeDrawer(navigation);
+    }
+
+    private boolean isMenuOpened() {
+        return drawer.isDrawerOpen(navigation);
+    }
+
+    private void openMenu() {
+
+        if (isFirstFocus) {
+            applyBlur();
+            if (Build.VERSION.SDK_INT > 16)
+                isFirstFocus = true;
+        }
+        blur.setVisibility(View.VISIBLE);
+
+        navigation.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
+        drawer.openDrawer(navigation);
+    }
+
+    @Override
+    public void reconn() {
+
+    }
+
+    private void blur(Bitmap bkg) {
+        Handler mHandler = new Handler() {
+            @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+            @Override
+            public void handleMessage(Message msg) {
+                blur.setBackground(new BitmapDrawable(MenuActivity.this.getResources(),
+                        (Bitmap) msg.obj));
+                right.destroyDrawingCache();
+            }
+        };
+        String str;
+        Bitmap overlay = BitMapUtil.convertToBlur(bkg, this);
+        Message msg = mHandler.obtainMessage(1, 1, 1, overlay);
+        mHandler.sendMessage(msg);
+    }
+
+    private void applyBlur() {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                right.setDrawingCacheEnabled(true);
+                right.buildDrawingCache();
+                Bitmap bitmap = right.getDrawingCache();
+
+                blur(bitmap);
+            }
+        };
+        thread.run();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isScrollbarFadingEnabled() && isMenuOpened())
+            closeMenu();
+        else
+            super.onBackPressed();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_MENU) {
+            if (isMenuOpened()) closeMenu();
+            else openMenu();
+        }
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(!QSModel.INSTANCE.loggedin()){
+            startActivity(new Intent(MenuActivity.this, U07RegisterActivity.class));
+            return;
+        }
+        switch (v.getId()) {
+            case R.id.navigation_btn_match:
+                startActivity(new Intent(MenuActivity.this, U01UserActivity.class));
+                finish();
+                break;
+            case R.id.navigation_btn_good_match:
+                startActivity(new Intent(MenuActivity.this, S18ShowByDateActivity.class));
+                finish();
+                break;
+            case R.id.u01_collection:
+                startActivity(new Intent(MenuActivity.this, U14CollectionActivity.class));
+                finish();
+                break;
+            case R.id.s17_settting:
+                startActivity(new Intent(MenuActivity.this, U02SettingsActivity.class));
+                finish();
+        }
+    }
+}
---
+++ app-android-native/app/src/main/java/com/focosee/qingshow/activity/fragment/MenuFragment.java
@@ -0,0 +1,175 @@
+package com.focosee.qingshow.activity.fragment;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.focosee.qingshow.R;
+import com.focosee.qingshow.activity.U02SettingsActivity;
+import com.focosee.qingshow.activity.U07RegisterActivity;
+import com.focosee.qingshow.activity.U14CollectionActivity;
+import com.focosee.qingshow.model.QSModel;
+import com.focosee.qingshow.util.BitMapUtil;
+
+import butterknife.InjectView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MenuFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MenuFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MenuFragment extends Fragment implements View.OnClickListener{
+
+    @InjectView(R.id.drawer)
+    DrawerLayout drawer;
+    @InjectView(R.id.navigation)
+    LinearLayout navigation;
+    @InjectView(R.id.blur)
+    ImageView blur;
+    @InjectView(R.id.context)
+    LinearLayout right;
+    @InjectView(R.id.s17_settting)
+    ImageView settingBtn;
+
+    private boolean isFirstFocus = true;
+
+    public MenuFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_menu, container, false);
+    }
+
+    private void initDrawer() {
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(getActivity(), drawer,
+                R.string.menu_open, R.string.menu_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                openMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                closeMenu();
+            }
+
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                if (slideOffset == 0.0) blur.setVisibility(View.INVISIBLE);
+            }
+        };
+        drawer.setDrawerListener(drawerToggle);
+    }
+
+    public void closeMenu() {
+        blur.setVisibility(View.INVISIBLE);
+        drawer.closeDrawer(navigation);
+    }
+
+    public boolean isMenuOpened() {
+        return drawer.isDrawerOpen(navigation);
+    }
+
+    public void openMenu() {
+
+        if (isFirstFocus) {
+            applyBlur();
+            if (Build.VERSION.SDK_INT > 16)
+                isFirstFocus = true;
+        }
+        blur.setVisibility(View.VISIBLE);
+
+        navigation.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
+        drawer.openDrawer(navigation);
+    }
+
+    private void blur(Bitmap bkg) {
+        Handler mHandler = new Handler() {
+            @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+            @Override
+            public void handleMessage(Message msg) {
+                blur.setBackground(new BitmapDrawable(getActivity().getResources(),
+                        (Bitmap) msg.obj));
+                right.destroyDrawingCache();
+            }
+        };
+        String str;
+        Bitmap overlay = BitMapUtil.convertToBlur(bkg, getActivity());
+        Message msg = mHandler.obtainMessage(1, 1, 1, overlay);
+        mHandler.sendMessage(msg);
+    }
+
+    private void applyBlur() {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                right.setDrawingCacheEnabled(true);
+                right.buildDrawingCache();
+                Bitmap bitmap = right.getDrawingCache();
+
+                blur(bitmap);
+            }
+        };
+        thread.run();
+    }
+
+    public boolean onBackPressed() {
+        if(drawer.isScrollbarFadingEnabled() && isMenuOpened()){
+            closeMenu();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(!QSModel.INSTANCE.loggedin()){
+            startActivity(new Intent(getActivity(), U07RegisterActivity.class));
+            return;
+        }
+        switch (v.getId()) {
+            case R.id.u01_collection:
+                startActivity(new Intent(getActivity(), U14CollectionActivity.class));
+                getActivity().finish();
+                break;
+            case R.id.s17_settting:
+                startActivity(new Intent(getActivity(), U02SettingsActivity.class));
+                getActivity().finish();
+        }
+    }
+
+}
