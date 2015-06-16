6a674c8cbc9d49ad2062d042cb1fbb9c653fcf1b
JACK1027 <13162929650@163.com>
2015-06-16 10:39:04 +0800

    fix getImgSrc(String url , int scale);

--- app-android-native/app/src/main/java/com/focosee/qingshow/util/ImgUtil.java
+++ app-android-native/app/src/main/java/com/focosee/qingshow/util/ImgUtil.java
@@ -2,32 +2,32 @@ package com.focosee.qingshow.util;
 
 public class ImgUtil {
 
-    public static String imgTo2x(String url){
+    public static String imgTo2x(String url) {
         String str = "";
-        if(null != url)
-        str = url.substring(0,url.length() - 4) + "@2x.jpg";
+        if (null != url)
+            str = url.substring(0, url.length() - 4) + "@2x.jpg";
         return str;
     }
 
-    public static String getImgSrc(String url,int scale){
-
-        String type=url.substring(url.length() -4);
+    public static String getImgSrc(String url, int scale) {
+        String[] array = url.split("\\.");
+        String type = "." + array[array.length - 1];
 
         String result = "";
-        switch (scale){
+        switch (scale) {
             case 1:
                 break;
             case 0:
-                result = url.substring(0,url.length() - 5) + "_s" + type;
+                result = url.replace(type, "_s" + type);
                 break;
             case -1:
-                result = url.substring(0,url.length() - 5) + "_xs" + type;
+                result = url.replace(type, "_xs" + type);
                 break;
             case -2:
-                result = url.substring(0,url.length() - 5) + "_xxs" + type;
+                result = url.replace(type, "_xxs" + type);
                 break;
             case -3:
-                result = url.substring(0,url.length() - 5) + "_xxxs" + type;
+                result = url.replace(type, "_xxxs" + type);
                 break;
         }
 
