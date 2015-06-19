c016a00707166d85501f96d374a9b45ba8c50610
mhy <mhy15287570@163.com>
2015-06-19 17:48:03 +0800

    add new util

---
+++ app-ios-native/qingshow-ios-native/model/entity/util/QSMatchShowUtil.m
@@ -0,0 +1,13 @@
+//
+//  QSMatchShowUtil.m
+//  qingshow-ios-native
+//
+//  Created by mhy on 15/6/19.
+//  Copyright (c) 2015年 QS. All rights reserved.
+//
+
+#import "QSMatchShowUtil.h"
+
+@implementation QSMatchShowUtil
+
+@end
