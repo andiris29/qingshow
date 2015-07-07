19d70a995b5ca95fe01f55a56a2ad5cdd40dff5a
Chenhr <rong5690001@gmail.com>
2015-07-07 17:42:13 +0800

    android-native:remove files

---
+++ app-android-native/app/src/main/java/com/focosee/qingshow/model/GoToWhereAfterLoginModel.java
@@ -0,0 +1,7 @@
+package com.focosee.qingshow.model;
+
+/**
+ * Created by Administrator on 2015/7/7.
+ */
+public class GoToWhereAfterLoginModel {
+}
---
+++ app-android-native/app/src/main/java/com/focosee/qingshow/widget/RecyclerView/SpacesItemDecoration.java
@@ -0,0 +1,7 @@
+package com.focosee.qingshow.widget.RecyclerView;
+
+/**
+ * Created by Administrator on 2015/7/7.
+ */
+public class SpacesItemDecoration {
+}
