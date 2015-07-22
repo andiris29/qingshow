13c7dfd3acd96f872342c3a03c0dd1b7d141df30
DylanJiang <jzhaolong@gmail.com>
2015-07-22 19:33:00 +0800

    adnroid-native:fix #910

--- app-android-native/app/src/main/java/com/focosee/qingshow/activity/QSPushActivity.java
+++ app-android-native/app/src/main/java/com/focosee/qingshow/activity/QSPushActivity.java
@@ -24,5 +24,57 @@ public class QSPushActivity extends Activity {
         super.onCreate(savedInstanceState);
     }
 
-   
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Bundle bundle = getIntent().getExtras();
+        String command = gotCommand(bundle);
+
+        Intent intent = null;
+        if (command.equals(QSPushAPI.NEW_SHOW_COMMENTS)) {
+            String id = gotCommentId(bundle);
+            intent = new Intent(QSPushActivity.this, S04CommentActivity.class);
+            intent.putExtra(S04CommentActivity.INPUT_SHOW_ID,id);
+        }
+
+
+        if (intent != null)
+            startActivity(intent);
+
+        this.finish();
+    }
+
+    private String gotCommand(Bundle bundle) {
+        String command = "";
+        for (String key : bundle.keySet()) {
+            if (key.equals(JPushInterface.EXTRA_EXTRA)) {
+                try {
+                    JSONObject json = new JSONObject(bundle.getString(key));
+                    command = json.get(QSPushAPI.EXTRA_KEY).toString();
+
+                    Log.d(TAG, "push command" + command);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return command;
+    }
+
+    private String gotCommentId(Bundle bundle) {
+        String id = "";
+        for (String key : bundle.keySet()) {
+            if (key.equals(JPushInterface.EXTRA_EXTRA)) {
+                try {
+                    JSONObject json = new JSONObject(bundle.getString(key));
+                    id = json.get("id").toString();
+                    Log.d(TAG, "push comment id" + id);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return id;
+    }
+
 }
