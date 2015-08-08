40493aa645bdf7eb71a266ea871efb5ce5afc2d8
andiris29 <andiris29@gmail.com>
2015-08-08 21:17:46 +0800

    Remove order in trade

--- admin-web/src/main/views/components/p04/StatusTo1Detail.js
+++ admin-web/src/main/views/components/p04/StatusTo1Detail.js
@@ -20,9 +20,7 @@ define([
             return;
         }
         return {
-            orders : [{
-                'actualPrice' : parseFloat(actualPrice)
-            }]
+            'actualPrice' : parseFloat(actualPrice)
         };
     };
 
