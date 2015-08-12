a3c2c6c2b62ecf89bd96e85117adfd7c57fed9c6
andiris29 <andiris29@gmail.com>
2015-08-12 14:27:39 +0800

    Update images

--- app-web/src/main/js/views/P01NotFound.js
+++ app-web/src/main/js/views/P01NotFound.js
@@ -5,6 +5,8 @@ define([
 // @formatter:on
     var P01NotFound = function(dom, initOptions) {
         P01NotFound.superclass.constructor.apply(this, arguments);
+        
+        this._dom$.text('404: ' + window.location.href);
     };
     violet.oo.extend(P01NotFound, violet.ui.ViewBase);
 
