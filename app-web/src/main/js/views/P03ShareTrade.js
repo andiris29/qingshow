3f7347320051b51ce3b968fec24d1951b9a52fc1
wxy325 <wxy325@qq.com>
2015-08-13 21:50:30 +0800

    app-web: bind trade info, adjust layout

--- app-web/src/main/js/views/P03ShareTrade.js
+++ app-web/src/main/js/views/P03ShareTrade.js
@@ -14,7 +14,20 @@ define([
                 var item = trade.itemSnapshot;
 
                 $('.p03-trade-item-cover', this._dom).css('background-image', violet.string.substitute('url({0})', item.thumbnail));
-//                $('.p02-show-cover-foreground', this._dom).css('background-image', violet.string.substitute('url({0})', show.coverForeground));
+                $('.p03-trade-item-title', this._dom)[0].innerText = item.name;
+                $('.p03-trade-item-price-number', this._dom)[0].innerText = '￥' + parseFloat(item.price).toFixed(2);
+                $('.p03-trade-item-tmall-price-number', this._dom)[0].innerText = '￥' + parseFloat(item.promoPrice).toFixed(2);
+
+                //p03-trade-item-qingshow-discount-number
+                //p03-trade-item-qingshow-price-number
+                if (trade.actualPrice) {
+                    var priceBefore = parseFloat(item.promoPrice || item.price);
+                    var actualPrice = parseFloat(trade.actualPrice);
+                    var discount = actualPrice * 100 / priceBefore;
+                    $('.p03-trade-item-qingshow-discount-number', this._dom)[0].innerText = discount.toFixed(2) + '%';
+                    $('.p03-trade-item-qingshow-price-number', this._dom)[0].innerText = '￥' + actualPrice.toFixed(2);
+                }
+
             }
         }.bind(this));
 
