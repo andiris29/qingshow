75ab5e05ace8f62fca6a24f24f148f4551f950b6
mhy <mhy15287570@163.com>
2015-08-13 23:26:54 +0800

    ios:fix share URL

--- app-ios-native/qingshow-ios-native/model/payment/QSPaymentService.m
+++ app-ios-native/qingshow-ios-native/model/payment/QSPaymentService.m
@@ -75,7 +75,7 @@
         //分享
         NSString* tradeId = [QSEntityUtil getIdOrEmptyStr:tradeDict];
         NSString* peopleId = [QSEntityUtil getIdOrEmptyStr:[QSTradeUtil getPeopleDic:tradeDict]];
-        [[QSShareService shareService] shareWithWechatMoment:@"正品折扣，在倾秀动动手指即刻拥有" desc:@"服装行业的最佳竞拍人，只要点击“我要折扣”，就可以以你心目中的价格轻松拥有心爱的宝贝哦！" image:[UIImage imageNamed:@"share_icon"] url:[NSString stringWithFormat:@"http://chingshow.com/app-web?entry=shareTrade&_id={%@}&initiatorRef={%@}",tradeId,peopleId] onSucceed:^{
+        [[QSShareService shareService] shareWithWechatMoment:@"正品折扣，在倾秀动动手指即刻拥有" desc:@"服装行业的最佳竞拍人，只要点击“我要折扣”，就可以以你心目中的价格轻松拥有心爱的宝贝哦！" image:[UIImage imageNamed:@"share_icon"] url:[NSString stringWithFormat:@"http://chingshow.com/app-web?entry=shareTrade&_id=%@&initiatorRef=%@",tradeId,peopleId] onSucceed:^{
             [SHARE_NW_ENGINE tradeShare:tradeDict onSucceed:succeedBlock onError:errorBlock];
         } onError:errorBlock];
     } else {
