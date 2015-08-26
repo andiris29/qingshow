0e394159e7a007c74770027f82032e652138856b
DylanJiang <jzhaolong@gmail.com>
2015-08-26 10:29:49 +0800

    android-native:clean projec t#1186

--- app-android-native/app/src/main/java/com/focosee/qingshow/activity/U06LoginActivity.java
+++ app-android-native/app/src/main/java/com/focosee/qingshow/activity/U06LoginActivity.java
@@ -158,28 +158,6 @@ public class U06LoginActivity extends BaseActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public void onResume() {
         super.onResume();
         MobclickAgent.onPageStart("U06Login"); //统计页面
