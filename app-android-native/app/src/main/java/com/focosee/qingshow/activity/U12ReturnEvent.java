93838d1de2c28e678b4cee81feac72de9b04df25
Chenhr <rong5690001@gmail.com>
2015-09-01 15:51:07 +0800

    android-native:update return

---
+++ app-android-native/app/src/main/java/com/focosee/qingshow/activity/U12ReturnEvent.java
@@ -0,0 +1,12 @@
+package com.focosee.qingshow.activity;
+
+/**
+ * Created by Administrator on 2015/9/1.
+ */
+public class U12ReturnEvent {
+
+    public int position;
+    public U12ReturnEvent(int position){
+        this.position = position;
+    }
+}
