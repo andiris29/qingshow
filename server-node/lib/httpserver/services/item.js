7c97094db261acb7d2aad5ebc5162d91b9114e49
Sebastian Qu <sebastianqu@gmail.com>
2015-09-03 13:28:39 +0800

    fix #1237

--- server-node/lib/httpserver/services/item.js
+++ server-node/lib/httpserver/services/item.js
@@ -3,9 +3,11 @@ var async = require('async');
 var _ = require('underscore');
 
 var Items = require('../../model/items');
+var jPushAudiences = require('../../model/jPushAudiences');
 
 var RequestHelper = require('../helpers/RequestHelper');
 var ResponseHelper = require('../helpers/ResponseHelper');
+var PushNotificationHelper = require('../helpers/PushNotificationHelper');
 
 var ServerError = require('../server-error');
 
@@ -33,6 +35,11 @@ item.updateExpectablePrice = {
             item.save(function(error, item) {
                 callback(error, item);
             });
+        }, function(item, callback) {
+            _itemPriceChanged(item, function() {
+                // do nothing
+            });
+            callback(null, item);
         }], function(error, item) {
             ResponseHelper.response(res, error, {
                 item : item
@@ -67,5 +74,46 @@ item.removeExpectablePrice = {
             });
         });
     }
+};
+
+var _itemPriceChanged = function(item, callback) {
+    async.waterfall([
+    function(callback) {
+        Trade.find({
+            'itemRef' : item._id,
+            'status' : 0
+        }).exec(callback);
+    },
+    function(trades, callback) {
+        var tasks = trades.map(function(trade) {
+            return function(cb) {
+                async.waterfall([
+                function(cb2) {
+                    jPushAudiences.find({
+                        peopleRef : trade.ownerRef
+                    }).exec(function(err, infos) {
+                        cb2(err, infos);
+                    });
+                },
+                function(infos, cb2) {
+                    var registrationIDs = [];
+                    infos.forEach(function(target) {
+                        registrationIDs.push(target.registrationId);
+                    });
+                    PushNotificationHelper.push(registrationIDs, PushNotificationHelper.MessageItemPriceChanged, {
+                        'command' : PushNotificationHelper.CommandItemPriceChanged,
+                        '_id' : req.body._id,
+                        '_tradeId' : trade._id.toString(),
+                        'actualPrice' : RequestHelper.parseNumber(req.body.actualPrice)
+                    }, cb2);
+                }], cb);
+            };
+        });
+        async.parallel(tasks, function(err) {
+            callback();
+        });
+    }], function(err) {
+        callback(err);
+    });
 };
 
