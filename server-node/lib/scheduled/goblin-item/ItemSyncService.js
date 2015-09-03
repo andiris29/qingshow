49f514665e52f6ae96e09991b5e6d7ab3882525c
wxy325 <wxy325@qq.com>
2015-09-03 22:31:29 +0800

    server-node: add ItemSyncService

---
+++ server-node/lib/scheduled/goblin-item/ItemSyncService.js
@@ -0,0 +1,160 @@
+var winston = require('winston');
+var TaobaoWebItem = require('./taobao/Item');
+var HmWebItem = require('./hm/Item');
+var JamyWebItem = require('./jamy/Item');
+var async = require('async');
+var URLParser = require('./URLParser');
+var _ = require('underscore');
+var Item = require('../../model/items');
+
+var ItemSyncService = {};
+
+/**
+ *
+ * @param item
+ * @param callback function(err, item)
+ */
+ItemSyncService.syncItem = function (item, callback) {
+    var crawlCallback = function (err) {
+        if (err) {
+            item.delist = new Date();
+            item.save(function (innerErr) {
+                setTimeout(function () {
+                    callback(innerErr || err, item);
+                }, _.random(5000, 10000));
+            });
+        } else {
+            setTimeout(function () {
+                callback(err, item);
+            }, _.random(5000, 10000));
+        }
+    };
+
+    _logItem('item start', item);
+    if (URLParser.isFromTaobao(item.source) || URLParser.isFromTmall(item.source)) {
+        _crawlItemTaobaoInfo(item, crawlCallback);
+    } else if (URLParser.isFromHm(item.source)) {
+        _crawlItemHmInfo(item, crawlCallback);
+    } else if (URLParser.isFromJamy(item.source)) {
+        _crawlItemJamyInfo(item, crawlCallback);
+    }
+
+};
+
+ItemSyncService.syncItemWithItemId = function (itemId, callback) {
+    async.waterfall([
+        function (callback) {
+            Item.findOne({
+                "_id" : itemId
+            }, callback);
+        }, function (item, callback) {
+            ItemSyncService.syncItem(item, callback);
+        }
+    ], callback);
+};
+
+
+ItemSyncService.canParseItemSource = function (itemSouceStr) {
+    if (!itemSouceStr) {
+        return false;
+    }
+    return URLParser.isFromTaobao(itemSouceStr) ||
+        URLParser.isFromTmall(itemSouceStr) ||
+        URLParser.isFromHm(itemSouceStr) ||
+        URLParser.isFromJamy(itemSouceStr);
+};
+
+
+
+var _crawlItemTaobaoInfo = function (item, callback) {
+    async.waterfall([
+        function (callback) {
+            TaobaoWebItem.getSkus(item.source, function (err, taobaoInfo) {
+                if (!taobaoInfo) {
+                    callback('invalidSource');
+                } else {
+                    callback(err, taobaoInfo);
+                }
+            });
+        }
+    ], function (err, taobaoInfo) {
+        if (err) {
+            _logItem('item error', item);
+            callback(err);
+        } else {
+            var delist = false;
+            if (!taobaoInfo || !Object.keys(taobaoInfo).length) {
+                callback('delist');
+                _logItem('item failed', item);
+            } else {
+                item.delist = null;
+                item.price = taobaoInfo.price;
+                item.promoPrice = taobaoInfo.promo_price;
+                item.skuProperties = taobaoInfo.skuProperties;
+                item.goblinUpdate = new Date();
+                _logItem('item success', item);
+                item.save(callback);
+            }
+
+        }
+    });
+};
+
+var _crawlItemHmInfo = function (item, callback) {
+    async.waterfall([function (callback) {
+        HmWebItem.getSkus(item.source, function(err, hmInfo) {
+            callback(err, hmInfo);
+        });
+    }], function (err, hmInfo) {
+        if (err) {
+            _logItem('item error', item);
+            callback(err);
+        } else {
+            if (!hmInfo || !Object.keys(hmInfo).length) {
+                _logItem('item failed', item);
+                callback('delist');
+            } else {
+                item.delist = null;
+                item.price = hmInfo.price;
+                item.promoPrice = hmInfo.promo_price;
+                item.skuProperties = hmInfo.skuProperties;
+                item.goblinUpdate = new Date();
+                _logItem('item success', item);
+                item.save(callback);
+            }
+
+        }
+    });
+};
+
+var _crawlItemJamyInfo = function (item, callback) {
+    async.waterfall([function (callback) {
+        JamyWebItem.getSkus(item.source, function (err, jamyInfo) {
+            callback(err, jamyInfo);
+        });
+    }], function (err, jamyInfo) {
+        if (err) {
+            _logItem('item error', item);
+            callback(err);
+        } else {
+            if (!jamyInfo || !Object.keys(jamyInfo).length) {
+                _logItem('item failed', item);
+                callback('delist');
+            } else {
+                item.delist = null;
+                item.price = jamyInfo.price;
+                item.promoPrice = jamyInfo.promo_price;
+                item.skuProperties = jamyInfo.skuProperties;
+                item.goblinUpdate = new Date();
+                _logItem('item success', item);
+                item.save(callback);
+            }
+        }
+    });
+};
+
+var _logItem = function (content, item) {
+    winston.info('[ItemSyncService] ' + content + ': ' + item._id);
+};
+
+module.exports = ItemSyncService;
--- server-node/lib/scheduled/goblin-item/run.js
+++ server-node/lib/scheduled/goblin-item/run.js
@@ -1,22 +1,20 @@
 var schedule = require('node-schedule');
-var winston = require('winston');
+
 var path = require('path');
 var fs = require('fs');
-
-var async = require('async');
 var winston = require('winston');
+var async = require('async');
 var _ = require('underscore');
 
-var TaobaoWebItem = require('./taobao/Item');
-var HmWebItem = require('./hm/Item');
-var JamyWebItem = require('./jamy/Item');
-var URLParser = require('./URLParser');
 
 var Item = require('../../model/items');
 // TODO Remove dependency on httpserver
 var ServerError = require('../../httpserver/server-error');
 var mongoose = require('mongoose');
 var qsmail = require('../../runtime/qsmail');
+var ItemSyncService = require('./ItemSyncService');
+
+
 
 var _next = function (time, config) {
     var report = "";
@@ -44,12 +42,10 @@ var _next = function (time, config) {
                     callback(ServerError.fromCode(ServerError.PagingNotExist));
                 } else {
                     items = items.filter(function (item) {
-                        if (!item.source) {
+                        if (!item) {
                             return false;
                         }
-
-                        return URLParser.isFromTaobao(item.source) || URLParser.isFromTmall(item.source) || 
-                                URLParser.isFromHm(item.source) || URLParser.isFromJamy(item.source);
+                        return ItemSyncService.canParseItemSource(item.source);
                     });
 
                     winston.info('[Goblin-tbitem] Total count: ' + items.length);
@@ -59,42 +55,20 @@ var _next = function (time, config) {
         },
         function (items, callback) {
             var tasks = [];
-
-
             items.forEach(function (item) {
-                if (!item) {
-                    return;
-                }
                 var task = function (callback) {
-                    var crawlCallback = function (err) {
+                    ItemSyncService.syncItem(item, function (err, item) {
                         var reportContent = [];
                         if (err) {
                             reportContent = [new Date(), item._id, err, null, item.source];
                             report += reportContent.join(',') + '\n';
-
-                            item.delist = new Date();
-                            item.save(function (err) {
-                                setTimeout(function () {
-                                    callback(err);
-                                }, _.random(5000, 10000));
-                            });
                         } else {
                             reportContent = [new Date(), item._id, 'success', 'promoPrice:' + item.promoPrice + ' price:' + item.price, item.source];
                             report += reportContent.join(',') + '\n';
-                            setTimeout(function () {
-                                callback(err);
-                            }, _.random(5000, 10000));
                         }
-                    };
-
-                    _logItem('item start', item);
-                    if (URLParser.isFromTaobao(item.source) || URLParser.isFromTmall(item.source)) {
-                        _crawlItemTaobaoInfo(item, crawlCallback);
-                    } else if (URLParser.isFromHm(item.source)) {
-                        _crawlItemHmInfo(item, crawlCallback);
-                    } else if (URLParser.isFromJamy(item.source)) {
-                        _crawlItemJamyInfo(item, crawlCallback);
-                    }
+                        //Goblin Item Will Only Record Error in Report
+                        callback();
+                    });
                 };
                 tasks.push(task);
             });
@@ -115,96 +89,6 @@ var _next = function (time, config) {
     });
 };
 
-var _crawlItemTaobaoInfo = function (item, callback) {
-    async.waterfall([
-        function (callback) {
-            TaobaoWebItem.getSkus(item.source, function (err, taobaoInfo) {
-                if (!taobaoInfo) {
-                    callback('invalidSource');
-                } else {
-                    callback(err, taobaoInfo);
-                }
-            });
-        }
-    ], function (err, taobaoInfo) {
-        if (err) {
-            _logItem('item error', item);
-            callback(err);
-        } else {
-            var delist = false;
-            if (!taobaoInfo || !Object.keys(taobaoInfo).length) {
-                callback('delist');
-                _logItem('item failed', item);
-            } else {
-                item.delist = null;
-                item.price = taobaoInfo.price;
-                item.promoPrice = taobaoInfo.promo_price;
-                item.skuProperties = taobaoInfo.skuProperties;
-                item.goblinUpdate = new Date();
-                _logItem('item success', item);
-                item.save(callback);
-            }
-
-        }
-    });
-};
-
-var _crawlItemHmInfo = function(item, callback) {
-    async.waterfall([function(callback) {
-        HmWebItem.getSkus(item.source, function(err, hmInfo) {
-            callback(err, hmInfo);
-        });
-    }], function(err, hmInfo) {
-        if (err) {
-            _logItem('item error', item);
-            callback(err);
-        } else {
-            if (!hmInfo || !Object.keys(hmInfo).length) {
-                _logItem('item failed', item);
-                callback('delist');
-            } else {
-                item.delist = null;
-                item.price = hmInfo.price;
-                item.promoPrice = hmInfo.promo_price;
-                item.skuProperties = hmInfo.skuProperties;
-                item.goblinUpdate = new Date();
-                _logItem('item success', item);
-                item.save(callback);
-            }
-
-        }
-    });
-};
-
-var _crawlItemJamyInfo = function(item, callback) {
-    async.waterfall([function(callback) {
-        JamyWebItem.getSkus(item.source, function(err, jamyInfo) {
-            callback(err, jamyInfo);
-        });
-    }], function(err, jamyInfo) {
-        if (err) {
-            _logItem('item error', item);
-            callback(err);
-        } else {
-            if (!jamyInfo || !Object.keys(jamyInfo).length) {
-                _logItem('item failed', item);
-                callback('delist');
-            } else {
-                item.delist = null;
-                item.price = jamyInfo.price;
-                item.promoPrice = jamyInfo.promo_price;
-                item.skuProperties = jamyInfo.skuProperties;
-                item.goblinUpdate = new Date();
-                _logItem('item success', item);
-                item.save(callback);
-            }
-        }
-    });
-};
-
-var _logItem = function (content, item) {
-    winston.info('[Goblin-tbitem] ' + content + ': ' + item._id);
-};
 
 var _run = function (config) {
     var startDate = new Date();
