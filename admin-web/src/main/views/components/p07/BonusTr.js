663341787a515410d9666004dd752476b56e5753
DylanJiang <jzhaolong@gmail.com>
2015-09-09 10:46:05 +0800

    admin-web:fix #1277

--- admin-web/src/main/views/components/p07/BonusTr.js
+++ admin-web/src/main/views/components/p07/BonusTr.js
@@ -6,43 +6,28 @@ define([
              View
 ) {
 // @formatter:on
+    var people = {};
     var BonusTr = function (dom, initOptions) {
         BonusTr.superclass.constructor.apply(this, arguments);
 
-        var people = initOptions.people;
-
+        people = initOptions.people;
         var td$ = $('td', this._dom);
 
         td$.eq(0).text(people._id);
-        td$.eq(1).text(people.nickname);
-
-        td$.eq(2).text(function () {
-            var totalBonus = 0;
-            people.bonuses.forEach(function (val) {
-                totalBonus += val.money;
-            });
-            return totalBonus;
-        });
-
-        td$.eq(3).text(getMoneyByStatus(0));
-        td$.eq(4).text(getMoneyByStatus(1));
-        td$.eq(5).text(getMoneyByStatus(2));
-
-        td$.eq(6).on('click', this._submit.bind(this));
-
-        function getMoneyByStatus(status) {
-            var money = 0;
-            people.bonuses.forEach(function (val) {
-                if (val.status === status)
-                    money += val.money;
-            });
-            return money;
-        }
+        td$.eq(1).text(people.id);
+        td$.eq(2).text(people.nickname);
+        td$.eq(3).text(people.count);
+        td$.eq(4).text(people.sum);
+        td$.eq(5).on('click', this._submit.bind(this));
 
     };
 
     BonusTr.prototype._submit = function () {
-        this.request('/userBonus/withdrawComplete', 'post', {}, function (err, metadata, data) {
+        this.request('/userBonus/withdrawComplete', 'post', {
+            '_id' : people._id,
+            'count' : people.count,
+            'sum' : people.sum
+        }, function (err, metadata, data) {
             if (err) {
                 alertify.error('提交失败');
             } else {
