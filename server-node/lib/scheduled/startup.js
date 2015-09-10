151cf755bc6d80e6a7f1d817566fb52d6fe4633c
wxy325 <wxy325@qq.com>
2015-09-10 22:55:37 +0800

    server-node: fix some error in GoblinScheduler and GoblinMainSlaver

--- server-node/lib/scheduled/startup.js
+++ server-node/lib/scheduled/startup.js
@@ -1,9 +1,12 @@
 var winston = require('winston');
 
 module.exports = function (config) {
-    require('./goblin/goblin-item/run')(config.goblin);
+    //require('./goblin/goblin-item/run')(config.goblin);
     require('./trade/autoReceiving')();
     require('./trade/notifyTradeInitialized')();
-    
+
+    require('./goblin/scheduler/GoblinScheduler').start();
+    require('./goblin/slaver/GoblinMainSlaver').start();
+
     winston.info('Startup scheduled success');
 };
