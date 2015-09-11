b590ff23b0c3104765c3817b65dd6c6d466f14ca
Chenhr <rong5690001@gmail.com>
2015-09-11 16:52:49 +0800

    android-native:update categories

--- app-android-native/app/src/main/java/com/focosee/qingshow/model/CategoriesModel.java
+++ app-android-native/app/src/main/java/com/focosee/qingshow/model/CategoriesModel.java
@@ -1,7 +1,25 @@
 package com.focosee.qingshow.model;
 
+import com.focosee.qingshow.model.vo.mongo.MongoCategories;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Administrator on 2015/9/11.
  */
-public class CategoriesModel {
+public enum CategoriesModel {
+    INSTANCE;
+
+    public Map<String, MongoCategories> categories;
+
+    public void setCategories(Map<String, MongoCategories> categories){
+        this.categories = categories;
+    }
+
+    public Map<String, MongoCategories> getCategories(){
+        if(null == this.categories)return null;
+        return categories;
+    }
+
 }
