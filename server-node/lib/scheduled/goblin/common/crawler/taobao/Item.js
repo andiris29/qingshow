3f30e8918eaa049a4e7733558faf5fbae051d2d9
andiris29 <andiris29@gmail.com>
2015-09-15 00:17:12 +0800

    Avoid dot in skuTable

--- server-node/lib/scheduled/goblin/common/crawler/taobao/Item.js
+++ server-node/lib/scheduled/goblin/common/crawler/taobao/Item.js
@@ -142,7 +142,7 @@ var generateTaobaoInfoFromSkus = function (skus){
         taobaoInfo.price = sku.price;
     }
     var skuNames = skus.map(function (sku) {
-        return sku.properties_name.split(';');
+        return sku.properties_name.replace(/\./g, '__').split(';');
     });
 
     if (skuNames.length && skus.length) {
