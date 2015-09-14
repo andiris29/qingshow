7c611963d27cc9f66f43a627b0dccf3b85ac57b6
wxy325 <wxy325@qq.com>
2015-09-15 01:57:50 +0800

    goblin: recover #1255

--- server-node/lib/scheduled/goblin/common/ItemSyncService.js
+++ server-node/lib/scheduled/goblin/common/ItemSyncService.js
@@ -7,6 +7,10 @@ var _ = require('underscore');
 var GoblinError = require('./GoblinError');
 
 var Item = require('../../../model/items');
+var People = require('../../../model/peoples');
+
+var crypto = require('crypto'), _secret = 'qingshow@secret';
+
 
 var ItemSyncService = {};
 
@@ -36,6 +40,38 @@ ItemSyncService.syncItem = function (item, callback) {
     async.waterfall([
         function (callback) {
             GoblinCrawler.crawl(item.source, callback);
+        }, function (webItem, callback) {
+            //taobaoInfo.shopId
+            var shopInfo = webItem.shopInfo;
+            if (shopInfo && shopInfo.shopId) {
+                var shopId = shopInfo.shopId;
+                async.waterfall([
+                    function (callback) {
+                        People.findOne({
+                            'shopInfo.taobao.sid' : shopInfo.shopId
+                        }, callback);
+                    }, function (people, callback) {
+                        if (!people) {
+                            new People({
+                                nickname: shopInfo.shopName,
+                                userInfo : {
+                                    id : shopId,
+                                    encryptedPassword : _encrypt(shopId)
+                                }
+                            }).save(callback);
+                        } else {
+                            callback(null, people);
+                        }
+                    }, function (people, number, callback) {
+                        item.shopRef = people._id;
+                        item.save(callback);
+                    }], function (err, i, count) {
+                    callback(null, webItem);
+                });
+            } else{
+                callback(null, webItem);
+            }
+
         }
     ], function (goblinError, webItem) {
         if (goblinError) {
@@ -55,10 +91,12 @@ ItemSyncService.syncItem = function (item, callback) {
             var skuTable = {};
             webItem.skuTable = webItem.skuTable || {};
             for (var key in webItem.skuTable) {
-                var value = webItem.skuTable[key];
-                // skuTable key should not contain '.' to avoid mongo error
-                key = key.replace(/\./g, '');
-                skuTable[key] = value;
+                if (webItem.skuTable.hasOwnProperty(key)) {
+                    var value = webItem.skuTable[key];
+                    // skuTable key should not contain '.' to avoid mongo error
+                    key = key.replace(/\./g, '');
+                    skuTable[key] = value;
+                }
             }
             item.skuTable = skuTable;
             _logItem('item success', item);
@@ -89,6 +127,14 @@ ItemSyncService.canParseItemSource = function (itemSourceStr) {
 
 var _logItem = function (content, item) {
     winston.info('[ItemSyncService] ' + content + ': ' + item._id);
+};
+
+//TODO add accountService, remove _encrypt
+var _encrypt = function(string) {
+    var cipher = crypto.createCipher('aes192', _secret);
+    var enc = cipher.update(string, 'utf8', 'hex');
+    enc += cipher.final('hex');
+    return enc;
 };
 
 module.exports = ItemSyncService;
