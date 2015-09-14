10231b3f6b113b323aecd3e0ff207c1d3dd57fd4
mhy <mhy15287570@163.com>
2015-09-15 01:42:28 +0800

    ios: fix #1364

--- app-ios-native/qingshow-ios-native/model/entity/util/QSItemUtil.m
+++ app-ios-native/qingshow-ios-native/model/entity/util/QSItemUtil.m
@@ -157,6 +157,8 @@
             }
             [returnStr appendString:subStr];
         }
+        NSRange range = [returnStr rangeOfString:@"."];
+        [returnStr deleteCharactersInRange:range];
         return returnStr;
     }
     return nil;
