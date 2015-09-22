9946579b16953b96b5a29e3ecf3005b2c4a731f8
Chenhr <rong5690001@gmail.com>
2015-09-22 10:40:47 +0800

    android-native:fix #1449

--- app-android-native/app/src/main/java/com/focosee/qingshow/adapter/U09TradeListAdapter.java
+++ app-android-native/app/src/main/java/com/focosee/qingshow/adapter/U09TradeListAdapter.java
@@ -112,6 +112,14 @@ public class U09TradeListAdapter extends AbsAdapter<MongoTrade> implements View.
             holder.setText(R.id.item_tradelist_description, trade.itemSnapshot.name);
             holder.setText(R.id.item_tradelist_exception, StringUtil.calculationException(trade.expectedPrice, trade.itemSnapshot.promoPrice));
             holder.setImgeByUrl(R.id.item_tradelist_image, trade.itemSnapshot.thumbnail);
+            holder.getView(R.id.item_tradelist_image).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(context, S10ItemDetailActivity.class);
+                    intent.putExtra(S10ItemDetailActivity.BONUSES_ITEMID, datas.get(position - 1).itemSnapshot._id);
+                    context.startActivity(intent);
+                }
+            });
             holder.setText(R.id.item_tradelist_actualPrice, StringUtil.FormatPrice(trade.itemSnapshot.promoPrice));
 
             holder.getView(R.id.item_tradelist_check).setOnClickListener(new View.OnClickListener() {
