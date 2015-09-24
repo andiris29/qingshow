8dcdcd75f992b72d9abf60b11742873e79151ff9
mhy <mhy15287570@163.com>
2015-09-24 12:05:11 +0800

    ios: add _ for comment pns

--- app-ios-native/qingshow-ios-native/pns/QSPnsHelper.m
+++ app-ios-native/qingshow-ios-native/pns/QSPnsHelper.m
@@ -33,7 +33,7 @@
     userInfoDict[@"fromBackground"] = @(fFromBackground);
     
     if ([command isEqualToString:kPnsCommandNewShowComments]) {
-        NSString* showId = [QSEntityUtil getStringValue:userInfo keyPath:@"id"];
+        NSString* showId = [QSEntityUtil getStringValue:userInfo keyPath:@"_id"];
         if (showId) {
             userInfoDict[@"showId"] = showId;
         }
