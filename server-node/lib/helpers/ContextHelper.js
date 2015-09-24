3a99356db45f2ed27dd56aafa947297ccaa8cd87
DylanJiang <jzhaolong@gmail.com>
2015-09-24 10:36:46 +0800

    server-node:clean trade.context.item and populate trade.itemRef

--- server-node/lib/helpers/ContextHelper.js
+++ server-node/lib/helpers/ContextHelper.js
@@ -101,15 +101,7 @@ ContextHelper.appendTradeContext = function(qsCurrentUserId, trades, callback) {
         _rInitiator(RPeopleShareTrade, qsCurrentUserId, trades, 'sharedByCurrentUser', callback);
     };
 
-    var itemDelist = function(callback) {
-        _item(Item, trades, 'delist', callback);
-    };
-
-    var itemExpectablePrice = function(callback) {
-        _item(Item, trades, 'expectablePrice', callback);
-    };
-
-    async.parallel([sharedByCurrentUser, itemDelist, itemExpectablePrice], function(err) {
+    async.parallel([sharedByCurrentUser], function(err) {
         callback(null, trades);
     });
 };
@@ -212,33 +204,3 @@ var _initiator = function(RModel, InitiatorModel, models, contextField, callback
         callback(null, models);
     });
 };
-
-var _item = function(InitiatorModel, models, contextField, callback) {
-    var tasks = models.map(function(model) {
-        return function(callback) {
-            if (!model.__context.item) {
-                model.__context.item = {};
-            }
-            model.__context.item[contextField] = {};                
-
-            InitiatorModel.findOne({
-                '_id' : model.itemRef
-            }, function(err , relationship) {
-                 if (Boolean(!err && relationship)) {
-                    console.log(relationship[contextField]);
-                    if (relationship[contextField]) { 
-                        model.__context.item[contextField] = relationship[contextField];
-                    };
-                   
-                    callback();
-                 } else {
-                    callback();
-                 }
-            })
-        }
-    });
-
-    async.parallel(tasks, function(err) {
-        callback(null, models);
-    })
-}
