f570a6edf0c591296908c8a1e2dc2f7e2fe9fa51
DylanJiang <jzhaolong@gmail.com>
2015-10-20 11:50:47 +0800

    node:fix logger

--- server-node/lib/helpers/TraceHelper.js
+++ server-node/lib/helpers/TraceHelper.js
@@ -6,7 +6,7 @@ var TraceHelper = module.exports;
 
 TraceHelper.trace = function(behavior, req, info){
 	var logger = require('../runtime/loggers').get(behavior);
-	logger.info(_.extend(RequestHelper.getClientInfo(req)), info, {
+	logger.info(_.extend(RequestHelper.getClientInfo(req), info, {
         'qsCurrentUserId' : req.qsCurrentUserId ? req.qsCurrentUserId.toString() : ''
-    });
+    }));
 };
