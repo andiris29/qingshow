70c22944d2b79bca47c86b696217da25133a45b4
DylanJiang <jzhaolong@gmail.com>
2015-10-28 10:58:49 +0800

    mongo:update

--- mongodb/2.1/feat-guide-001_#1828.js
+++ mongodb/2.1/feat-guide-001_#1828.js
@@ -1,1 +1,3 @@
 db.getCollection('peoples').update({"mobile" : {$exists: false}}, {$set : {"role" : 0}}, false, true)
+
+db.getCollection('peoples').update({"nickname" : /u\d{6}/}, {$set : {role : 0}}, false, true)
