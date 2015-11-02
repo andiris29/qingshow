e10b31aa703c26324d659f2edbda6c9a72b63a8c
andiris29 <andiris29@gmail.com>
2015-11-03 01:31:17 +0800

    Revert "node:remove urlparser"
    This reverts commit 33bbbee2c0195b95cf78a9aede4ab9a4c853781c.

--- server-node-proxy/startup.js
+++ server-node-proxy/startup.js
@@ -2,14 +2,17 @@ var express =require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
 var Iconv = require('iconv-lite');
+var URLParser = require('./URLParser');
 
 var app = express();
 app.listen('30003');
 app.use(bodyParser.json());
 app.post('/taobao/get', function(req, res){
 	var qsParam = req.body;
+	var url = 'http://item.taobao.com/item.htm?id=' + URLParser.getIidFromSource(qsParam.url);
+	console.log(url);
 	request.get({
-		'url' : qsParam.url,
+		'url' : url,
 		'headers' : qsParam.headers,
 		'encoding' : 'binary'
 	}, function(err, response, body){
