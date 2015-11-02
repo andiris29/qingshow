33bbbee2c0195b95cf78a9aede4ab9a4c853781c
DylanJiang <jzhaolong@gmail.com>
2015-11-03 00:56:50 +0800

    node:remove urlparser

--- server-node-proxy/startup.js
+++ server-node-proxy/startup.js
@@ -2,17 +2,14 @@ var express =require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
 var Iconv = require('iconv-lite');
-var URLParser = require('./URLParser');
 
 var app = express();
 app.listen('30003');
 app.use(bodyParser.json());
 app.post('/taobao/get', function(req, res){
 	var qsParam = req.body;
-	var url = 'http://item.taobao.com/item.htm?id=' + URLParser.getIidFromSource(qsParam.url);
-	console.log(url);
 	request.get({
-		'url' : url,
+		'url' : qsParam.url,
 		'headers' : qsParam.headers,
 		'encoding' : 'binary'
 	}, function(err, response, body){
