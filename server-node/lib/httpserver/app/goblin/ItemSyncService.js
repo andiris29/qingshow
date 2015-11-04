105cc72787121b6aa9110b1ba172fc2883015325
andiris29 <andiris29@gmail.com>
2015-11-04 10:30:58 +0800

    Update outdate to 30 days

--- server-node/lib/httpserver/app/goblin/ItemSyncService.js
+++ server-node/lib/httpserver/app/goblin/ItemSyncService.js
@@ -22,7 +22,7 @@ ItemSyncService.isOutDate = function (item) {
     return ((now - item.sync) > ItemSyncService.outDateDuration);
 };
 
-ItemSyncService.outDateDuration = 1000 * 60 * 60 * 24;
+ItemSyncService.outDateDuration = 1000 * 60 * 60 * 24 * 30;
 
 ItemSyncService.syncItemInfo = function(item, itemInfo, err, callback) {
     if (err && _.isString(err)) {
