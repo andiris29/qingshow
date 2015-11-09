36f5ad14966d002cfa5bc0365e389fe51915de94
wxy325 <wxy325@qq.com>
2015-11-09 22:48:41 +0800

    server-node: fix #1960

--- server-node/lib/dbmodels/Show.js
+++ server-node/lib/dbmodels/Show.js
@@ -1,5 +1,10 @@
 var mongoose = require('mongoose');
 var textSearch = require('mongoose-text-search');
+var async = require('async');
+
+var Item = require('./Item');
+var Category = require('./Category');
+
 
 var Schema = mongoose.Schema;
 var showSchema;
@@ -43,6 +48,47 @@ showSchema = Schema({
     categoryNames : [String]
 });
 
+showSchema.post('save', function (doc) {
+    if (!doc.categoryNames || !doc.categoryNames.length) {
+        var itemIds = doc.itemRefs || [];
+        async.waterfall([
+            function (callback) {
+                Item.find({
+                    '_id' : {
+                        '$in' : itemIds
+                    }
+                }, callback);
+            }, function (items, callback) {
+                items = items || [];
+                var categoryIds = items.map(function (i) {
+                    return i.categoryRef;
+                }).filter(function (c) {
+                    return !!c;
+                });
+                Category.find({
+                    '_id' : {
+                        '$in' : categoryIds
+                    }
+                }, callback);
+            }, function (categories, callback) {
+                var categoriyNames = categories.map(function (c) {
+                    return c.name;
+                });
+                categoriyNames = categoriyNames.filter(function (n, index) {
+                    return categoriyNames.indexOf(n) === index;
+                });
+                doc.categoryNames = categoriyNames;
+                if (categoriyNames.length) {
+                    doc.save(callback);
+                }
+            }
+        ], function (err) {
+            console.log(err);
+        });
+    }
+});
+
+
 showSchema.plugin(textSearch);
 
 showSchema.index({
@@ -53,6 +99,9 @@ showSchema.index({
         categoryNames : 1
     }
 });
+
+
+
 
 var Show = mongoose.model('shows', showSchema);
 module.exports = Show;
