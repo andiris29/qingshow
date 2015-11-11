d0ff84197b05bb0d387a10a4a99fa4313d3bf799
wxy325 <wxy325@qq.com>
2015-11-12 03:07:09 +0800

    ios: add #1958

---
+++ app-ios-native/qingshow-ios-native/vc/matchshows/QSS23SearchViewController.h
@@ -0,0 +1,15 @@
+//
+//  QSS23SearchViewController.h
+//  qingshow-ios-native
+//
+//  Created by wxy325 on 15/11/12.
+//  Copyright (c) 2015年 QS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QSS23SearchViewController : UIViewController
+
+- (instancetype)init;
+
+@end
