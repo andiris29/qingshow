8eed398f7302a80779bf749e967ab26a6990d44e
wxy325 <wxy325@qq.com>
2015-11-21 23:59:41 +0800

    ios: add bind mobile view controller

---
+++ app-ios-native/qingshow-ios-native/vc/user/login/QSBindMobileViewController.h
@@ -0,0 +1,15 @@
+//
+//  QSBindMobileViewController.h
+//  qingshow-ios-native
+//
+//  Created by wxy325 on 15/11/21.
+//  Copyright (c) 2015年 QS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QSBindMobileViewController : UIViewController
+
+- (instancetype)init;
+
+@end
